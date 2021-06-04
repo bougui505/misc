e521eb7215d9616420d548fe45f5b534a2429cc9
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2021-06-04 17:20:55 +0200

    new file:   protein_roll.py

---
+++ python/protein_roll.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: UTF8 -*-
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2021 Institut Pasteur                                       #
+#                 				                            #
+#                                                                           #
+#  Redistribution and use in source and binary forms, with or without       #
+#  modification, are permitted provided that the following conditions       #
+#  are met:                                                                 #
+#                                                                           #
+#  1. Redistributions of source code must retain the above copyright        #
+#  notice, this list of conditions and the following disclaimer.            #
+#  2. Redistributions in binary form must reproduce the above copyright     #
+#  notice, this list of conditions and the following disclaimer in the      #
+#  documentation and/or other materials provided with the distribution.     #
+#  3. Neither the name of the copyright holder nor the names of its         #
+#  contributors may be used to endorse or promote products derived from     #
+#  this software without specific prior written permission.                 #
+#                                                                           #
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
+#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
+#                                                                           #
+#  This program is free software: you can redistribute it and/or modify     #
+#                                                                           #
+#############################################################################
+
+
+from scipy.spatial.transform import Rotation as R
+import numpy as np
+from pymol import cmd
+import psico.fullinit
+import os
+
+
+if __name__ == '__main__':
+    import argparse
+    # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
+    parser = argparse.ArgumentParser(description='')
+    # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
+    parser.add_argument('-p', '--pdb')
+    parser.add_argument('--random', action='store_true', help='Random rotations')
+    parser.add_argument('--nframes', type=int, default=1000)
+    args = parser.parse_args()
+
+    cmd.set('retain_order', 1)
+
+    cmd.load(args.pdb, 'inpdb')
+    coords = cmd.get_coords('inpdb')
+    coords -= coords.mean(axis=0)
+
+    angles = np.linspace(0, 2 * np.pi, args.nframes // 3)
+    i = 0
+    for vec in [np.array([1, 0, 0]), np.array([0, 1, 0]), np.array([0, 0, 1])]:
+        for alpha in angles:
+            i += 1
+            r = R.from_rotvec(alpha * vec).as_matrix()
+            coords_rot = (r.dot(coords.T)).T
+            cmd.create('out', 'inpdb', 1, i)
+            cmd.load_coords(coords_rot, 'out', state=i)
+    outname = f'{os.path.splitext(args.pdb)[0]}.dcd'
+    cmd.save_traj(outname, 'out')
