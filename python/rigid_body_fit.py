33bb6d3c9932695e1e2b2cade85027362525fe1f
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2021-06-18 14:07:14 +0200

    new file:   rigid_body_fit.py Python/numpy implementation of rigid body protein alignment Kabsch algorithm

---
+++ python/rigid_body_fit.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: UTF8 -*-
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2021 Institut Pasteur                                       #
+#                 				                            #
+#                                                                           #
+#  Redistribution and use in source and binary forms, with or without       #
+#  modification, are permitted provided that the following conditions       #
+#  are met:                                                                 #
+#                                                                           #
+#  1. Redistributions of source code must retain the above copyright        #
+#  notice, this list of conditions and the following disclaimer.            #
+#  2. Redistributions in binary form must reproduce the above copyright     #
+#  notice, this list of conditions and the following disclaimer in the      #
+#  documentation and/or other materials provided with the distribution.     #
+#  3. Neither the name of the copyright holder nor the names of its         #
+#  contributors may be used to endorse or promote products derived from     #
+#  this software without specific prior written permission.                 #
+#                                                                           #
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
+#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
+#                                                                           #
+#  This program is free software: you can redistribute it and/or modify     #
+#                                                                           #
+#############################################################################
+
+
+import numpy as np
+
+
+def rigid_body_fit(A, B):
+    """
+    See: https://en.wikipedia.org/wiki/Kabsch_algorithm
+    2-D or 3-D registration with known correspondences.
+    Registration occurs in the zero centered coordinate system, and then
+    must be transported back.
+        Args:
+        -    A: Numpy array of shape (N,D) -- Point Cloud to Align (source)
+        -    B: Numpy array of shape (N,D) -- Reference Point Cloud (target)
+        Returns:
+        -    R: optimal rotation
+        -    t: optimal translation
+    """
+    a_mean = A.mean(axis=0)
+    b_mean = B.mean(axis=0)
+    A_c = A - a_mean
+    B_c = B - b_mean
+    # Covariance matrix
+    H = A_c.T.dot(B_c)
+    U, S, Vt = np.linalg.svd(H)
+    V = Vt.T
+    # Rotation matrix
+    R = V.dot(U.T)
+    # Translation vector
+    t = b_mean - R.dot(a_mean)
+    return R, t
+
+
+if __name__ == '__main__':
+    import argparse
+    from pymol import cmd
+    # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
+    parser = argparse.ArgumentParser(description='')
+    # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
+    parser.add_argument('--pdb1')
+    parser.add_argument('--pdb2', help='PDB filename for the reference structure to fit on')
+    parser.add_argument('--sel1', help='Selection for pdb1 (default: all)', default='all')
+    parser.add_argument('--sel2', help='Selection for pdb2 (equal to sel1 if not given)')
+    args = parser.parse_args()
+
+    cmd.load(args.pdb1, 'pdb1')
+    cmd.load(args.pdb2, 'pdb2')
+    if args.sel2 is None:
+        args.sel2 = args.sel1
+    coords1 = cmd.get_coords(f'pdb1 and {args.sel1}')
+    coords2 = cmd.get_coords(f'pdb2 and {args.sel2}')
+    R, t = rigid_body_fit(coords1, coords2)
+    print(R)
+    coords_aligned = (R.dot(coords1.T)).T + t
+    cmd.load_coords(coords_aligned, f'pdb1 and {args.sel1}')
+    cmd.save('aligned.pdb', 'pdb1')
