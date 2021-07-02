e6b67e03c60e40a087b94d4e8316675cc60b68ab
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2021-07-02 17:43:47 +0200

    Help added

--- python/Grid3/mrc.py
+++ python/Grid3/mrc.py
@@ -69,15 +69,15 @@ if __name__ == '__main__':
     # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
     parser = argparse.ArgumentParser(description='')
     # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
-    parser.add_argument('-t', '--tensor')
-    parser.add_argument('-o', '--origin', type=float, nargs='+', default=[0, 0, 0])  # 10. 20. 30.
-    parser.add_argument('-s', '--spacing', type=float, default=1)  # 1.
-    parser.add_argument('--out')  # 10. 20. 30.
-    parser.add_argument('--mrc')
+    parser.add_argument('--npy', help='Read a npy file and create a MRC file (see: --out)')
+    parser.add_argument('--origin', type=float, nargs='+', default=[0, 0, 0], help='Origin for the MRC file')  # 10. 20. 30.
+    parser.add_argument('--spacing', type=float, default=1, help='Spacing for the MRC file')  # 1.
+    parser.add_argument('--out', help='Out MRC file name (see: --npy)')  # 10. 20. 30.
+    parser.add_argument('--mrc', help='Read the given MRC and print the flatten data to stdout')
     args = parser.parse_args()
 
-    if args.tensor is not None:
-        data = np.load(args.tensor)
+    if args.npy is not None:
+        data = np.load(args.npy)
         save_density(data, args.out, args.spacing, args.origin, 0)
     if args.mrc is not None:
         data = mrc_to_array(args.mrc)
