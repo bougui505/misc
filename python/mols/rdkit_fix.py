bf9209a5c76c169df39465ae4108b104580ab13a
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2021-07-07 09:10:24 +0200

    Can handle multiple files 	modified:   rdkit_fix.py

--- python/mols/rdkit_fix.py
+++ python/mols/rdkit_fix.py
@@ -24,25 +24,35 @@ if __name__ == '__main__':
     # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
     parser = argparse.ArgumentParser(description='')
     # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
-    parser.add_argument('-m', '--mol', help='input mol file to fix')
-    parser.add_argument('-s', '--smiles', help='input smiles file to convert in 3D and fix')
+    parser.add_argument('-m', '--mol', help='input mol file to fix. Multiple mol files can be given.', nargs='+')
+    parser.add_argument('-s', '--smiles', help='input smiles file to convert in 3D and fix. Multiple smiles can be given.', nargs='+')
     parser.add_argument('-t', '--tautomers', help='generate all tautomers', action='store_true')
     args = parser.parse_args()
 
+    mlist = []
+    outnames = []
     if args.mol is not None:
-        m = Chem.MolFromMolFile(args.mol)
-        outname = os.path.splitext(args.mol)[0] + ".sdf"
+        for mol in args.mol:
+            m = Chem.MolFromMolFile(mol)
+            outname = os.path.splitext(mol)[0] + ".sdf"
+            mlist.append(m)
+            outnames.append(outname)
     if args.smiles is not None:
-        m = Chem.MolFromSmiles(args.smiles)
-        outname = args.smiles + ".sdf"
+        for smiles in args.smiles:
+            m = Chem.MolFromSmiles(smiles)
+            outname = smiles + ".sdf"
+            mlist.append(m)
+            outnames.append(outname)
     if args.tautomers:
-        enumerator = rdMolStandardize.TautomerEnumerator()
-        tauts = enumerator.Enumerate(m)
-        w = Chem.SDWriter(outname)
-        for i, taut in enumerate(tauts):
-            taut = fixmol(taut)
-            w.write(taut)
+        for m, outname in zip(mlist, outnames):
+            enumerator = rdMolStandardize.TautomerEnumerator()
+            tauts = enumerator.Enumerate(m)
+            w = Chem.SDWriter(outname)
+            for i, taut in enumerate(tauts):
+                taut = fixmol(taut)
+                w.write(taut)
         sys.exit(0)
-    m = fixmol(m)
-    w = Chem.SDWriter(outname)
-    w.write(m)
+    for m, outname in zip(mlist, outnames):
+        m = fixmol(m)
+        w = Chem.SDWriter(outname)
+        w.write(m)
