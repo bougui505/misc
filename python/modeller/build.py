e7c41622d137f4c35a061301412c58fe7203e541
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2021-09-23 16:23:11 +0200

    modified:   build.py

--- python/modeller/build.py
+++ python/modeller/build.py
@@ -37,12 +37,15 @@
 #############################################################################
 
 import modeller
+from modeller.optimizers import ConjugateGradients
 
 lib = '/usr/lib/modeller10.1/modlib'
 
 env = modeller.Environ()
 env.libs.topology.read(file=f'{lib}/top_heav.lib')
 env.libs.parameters.read(file=f'{lib}/par.lib')
+
+modeller.log.none()
 
 
 def extended(sequence):
@@ -61,7 +64,64 @@ def extended(sequence):
     return mdl
 
 
+class System():
+    """
+    Build a protein system by adding residues sequentially
+    """
+    def __init__(self):
+        """
+        >>> system = System()
+        >>> system.add_residue('A')
+        >>> system.add_residue('D')
+        >>> system.build()
+        Model containing 1 chain, 2 residues, and 14 atoms
+        >>> system.energy
+        40331.953125
+        """
+        self.mdl = modeller.Model(env)
+        self.sequence = ''
+
+    def add_residue(self, resname):
+        """
+        Add the given residue (given by resname) to the system
+
+        Args:
+            resname: Residue name to add in 1-letter code (str)
+
+        """
+        self.sequence += resname
+        aln = modeller.Alignment(env)
+        aln.append_sequence(self.sequence)
+        self.mdl.clear_topology()
+        self.mdl.generate_topology(aln[0])
+
+    def build(self):
+        """
+        Build the atomic coordinates of the system
+
+        Returns:
+            mdl: Modeller Model object
+
+        """
+        self.mdl.build(build_method='INTERNAL_COORDINATES',
+                       initialize_xyz=False)
+        return self.mdl
+
+    @property
+    def energy(self):
+        atmsel = modeller.Selection(self.mdl)
+        energy, terms = atmsel.energy(output='NO_REPORT', file='energy.log')
+        return energy
+
+    def minimize(self):
+        cg = ConjugateGradients()
+        atmsel = modeller.Selection(self.mdl)
+        cg.optimize(atmsel, max_iterations=20)
+
+
 if __name__ == '__main__':
+    import sys
+    import doctest
     import argparse
     # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
     parser = argparse.ArgumentParser(
@@ -69,7 +129,14 @@ if __name__ == '__main__':
     # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
     parser.add_argument('-s', '--seq', help='1-letter code sequence to build')
     parser.add_argument('-o', '--out', help='out pdb file name')
+    parser.add_argument('--test',
+                        help='Test System class',
+                        action='store_true')
     args = parser.parse_args()
+
+    if args.test:
+        doctest.testmod()
+        sys.exit()
 
     mdl = extended(args.seq)
     mdl.write(args.out)
