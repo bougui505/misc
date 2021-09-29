dc6278c2ff0d172a43692cd0a6ccfded0e1a5bd5
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2021-09-29 17:44:02 +0200

    modified:   basis.py: origin added

--- python/basis.py
+++ python/basis.py
@@ -52,18 +52,19 @@ class Basis():
         dim: dimension of the space
 
     """
-    def __init__(self, u, v, w):
+    def __init__(self, u, v, w, origin=np.zeros(3)):
         """
 
         Args:
             u: x axis of new basis in the old basis
             v: y axis of new basis in the old basis
             w: z axis of new basis in the old basis
+            origin: origin of the new basis in the old basis coordinates
 
         >>> u = [1, 0, 0]
         >>> v = [0, 1, 0]
         >>> w = [0, 0, 1]
-        >>> basis = Basis(v, w, u)
+        >>> basis = Basis(v, w, u, origin=(3, 4, 5))
         >>> coords = np.arange(15).reshape((5, 3))
         >>> coords
         array([[ 0,  1,  2],
@@ -73,11 +74,11 @@ class Basis():
                [12, 13, 14]])
         >>> coords_new = basis.change(coords)
         >>> coords_new
-        array([[ 1.,  2.,  0.],
-               [ 4.,  5.,  3.],
-               [ 7.,  8.,  6.],
-               [10., 11.,  9.],
-               [13., 14., 12.]])
+        array([[-3., -3., -3.],
+               [ 0.,  0.,  0.],
+               [ 3.,  3.,  3.],
+               [ 6.,  6.,  6.],
+               [ 9.,  9.,  9.]])
         >>> coords_back = basis.back(coords_new)
         >>> (coords_back == coords).all()
         True
@@ -89,6 +90,8 @@ class Basis():
         assert (self.A.T.dot(self.A) == np.identity(
             self.dim)).all(), "u, v, w are not an orthonormal basis"
         self.A_inv = np.linalg.inv(self.A)
+        self.origin = np.asarray(origin)[None, ...]
+        self.origin_new = self.A_inv.dot(self.origin.T).T
 
     def change(self, coords):
         """
@@ -97,16 +100,17 @@ class Basis():
             coords: Coordinates of points in the old basis (shape: (n, self.dim))
 
         """
-        coords_new = self.A_inv.dot(coords.T).T
+        coords_new = self.A_inv.dot(coords.T).T - self.origin_new
         return coords_new
 
     def back(self, coords):
         """
 
         Args:
-            coords: Coordinates of points in the old basis (shape: (n, self.dim))
+            coords: Coordinates of points in the new basis (shape: (n, self.dim))
 
         """
+        coords += self.origin_new
         coords_new = self.A.dot(coords.T).T
         return coords_new
 
