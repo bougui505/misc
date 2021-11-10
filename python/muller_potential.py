371c573fc2febcf5c3c4ab9d740d1c008474d093
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2021-11-10 10:37:26 +0100

    new file:   muller_potential.py

---
+++ python/muller_potential.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: UTF8 -*-
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2021 Institut Pasteur                                       #
+#                 				                            #
+#                                                                           #
+#  Redistribution and use in source and binary forms, with or without       #
+#  modification, are permitted provided that the following conditions       #
+#  are met:                                                                 #
+#                                                                           #
+#  1. Redistributions of source code must retain the above copyright        #
+#  notice, this list of conditions and the following disclaimer.            #
+#  2. Redistributions in binary form must reproduce the above copyright     #
+#  notice, this list of conditions and the following disclaimer in the      #
+#  documentation and/or other materials provided with the distribution.     #
+#  3. Neither the name of the copyright holder nor the names of its         #
+#  contributors may be used to endorse or promote products derived from     #
+#  this software without specific prior written permission.                 #
+#                                                                           #
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
+#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
+#                                                                           #
+#  This program is free software: you can redistribute it and/or modify     #
+#                                                                           #
+#############################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def muller_potential(x, y):
+    """Muller potential
+    Parameters
+    ----------
+    x : {float, np.ndarray}
+    X coordinate. If you supply an array, x and y need to be the same shape,
+    and the potential will be calculated at each (x,y pair)
+    y : {float, np.ndarray}
+    Y coordinate. If you supply an array, x and y need to be the same shape,
+    and the potential will be calculated at each (x,y pair)
+    Returns
+    -------
+    potential : {float, np.ndarray, or theano symbolic variable}
+    Potential energy. Will be the same shape as the inputs, x and y.
+    Reference
+    ---------
+    Code adapted from https://cims.nyu.edu/~eve2/ztsMueller.m
+    """
+    aa = [-1, -1, -6.5, 0.7]
+    bb = [0, 0, 11, 0.6]
+    cc = [-10, -10, -6.5, 0.7]
+    AA = [-200, -100, -170, 15]
+    XX = [1, 0, -0.5, -1]
+    YY = [0, 0.5, 1.5, 1]
+    # use symbolic algebra if you supply symbolic quantities
+    value = 0
+    for j in range(0, 4):
+        value += AA[j] * np.exp(aa[j] * (x - XX[j])**2 + bb[j] * (x - XX[j]) *
+                                (y - YY[j]) + cc[j] * (y - YY[j])**2)
+    return value
+
+
+if __name__ == '__main__':
+    import argparse
+    # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
+    parser = argparse.ArgumentParser(description='')
+    # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
+    parser.add_argument('-a', '--arg1')
+    args = parser.parse_args()
+
+    minx = -1.5
+    maxx = 1.2
+    miny = -0.2
+    maxy = 2
+    ax = None
+    grid_width = max(maxx - minx, maxy - miny) / 50.0
+    xx, yy = np.mgrid[minx:maxx:grid_width, miny:maxy:grid_width]
+    V = muller_potential(xx, yy)
+    V = np.ma.masked_array(V, V > 200)
+    plt.contourf(V, 40)
+    plt.colorbar()
+    plt.show()
