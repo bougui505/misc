be6785cd5d650891806208bc48f1ce22c35ec50b
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2021-11-29 15:10:22 +0100

    modified:   mullersac.py

--- python/gym/mullersac.py
+++ python/gym/mullersac.py
@@ -20,7 +20,7 @@ model = SAC(
     train_freq=(1, "episode"),
     # ent_coef=0.1,
     policy_kwargs=policy_kwargs,
-    use_sde=True,
+    use_sde=False,
     sde_sample_freq=100)
 model.learn(total_timesteps=100000, log_interval=1)
 model.save("sac_muller")
