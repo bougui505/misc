83c95b5f210ed86d19f52b2854df85bdba1c3781
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2022-03-15 10:48:15 +0100

    new file:   getpdb.py

---
+++ python/protein/getpdb.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: UTF8 -*-
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2022 Institut Pasteur                                       #
+#                 				                            #
+#                                                                           #
+#  Redistribution and use in source and binary forms, with or without       #
+#  modification, are permitted provided that the following conditions       #
+#  are met:                                                                 #
+#                                                                           #
+#  1. Redistributions of source code must retain the above copyright        #
+#  notice, this list of conditions and the following disclaimer.            #
+#  2. Redistributions in binary form must reproduce the above copyright     #
+#  notice, this list of conditions and the following disclaimer in the      #
+#  documentation and/or other materials provided with the distribution.     #
+#  3. Neither the name of the copyright holder nor the names of its         #
+#  contributors may be used to endorse or promote products derived from     #
+#  this software without specific prior written permission.                 #
+#                                                                           #
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
+#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
+#                                                                           #
+#  This program is free software: you can redistribute it and/or modify     #
+#                                                                           #
+#############################################################################
+
+from pymol import cmd
+import os
+
+cmd.set('fetch_path', os.path.expanduser('~/pdb'))
+
+
+def clean_sel(selstr):
+    filters = [('polymer.protein', ''), ('and ', ''), ('or ', '+'), ('all', ''), ('chain ', '')]
+    out = selstr
+    for f in filters:
+        i, o = f
+        out = out.replace(i, o)
+        out = out.strip()
+    # out = out.replace(' ', '_')
+    return out
+
+
+if __name__ == '__main__':
+    import sys
+    import doctest
+    import argparse
+    # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
+    parser = argparse.ArgumentParser(description='')
+    # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
+    parser.add_argument('-p', '--pdb', help='pdb code to download', required=True)
+    parser.add_argument('-s', '--sel', help='Selection to save in file')
+    parser.add_argument('--out', help='Out file name (default: pdbcode.pdb if no selection given)')
+    parser.add_argument('--test', help='Test the code', action='store_true')
+    args = parser.parse_args()
+
+    if args.test:
+        doctest.testmod(optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE)
+        sys.exit()
+
+    cmd.fetch(args.pdb, name='pdb')
+    if args.sel is None:
+        args.sel = 'all'
+    if args.out is None:
+        suffix = clean_sel(args.sel)
+        if len(suffix) == 0:
+            args.out = f'{args.pdb.lower()}.pdb'
+        else:
+            args.out = f'{args.pdb.lower()}_{suffix}.pdb'
+    cmd.save(filename=args.out, selection=args.sel)
