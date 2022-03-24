587b3818660f9824e5366833050bbd111da83ab2
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2022-03-24 13:34:29 +0100

    modified:   molconvert.py

--- python/mols/molconvert.py
+++ python/mols/molconvert.py
@@ -44,12 +44,30 @@ if __name__ == '__main__':
     # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
     parser = argparse.ArgumentParser(description='General format conversion for molecular structure files using PyMol')
     # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
-    parser.add_argument('-i', '--inp', help='Input file. Can read multiple files and globbing', required=True, nargs='+')
-    parser.add_argument('-o', '--out', help='Output FORMAT (given as EXTENSION). The output filename will be the same filename with the given extension', required=True)
-    parser.add_argument('--hetatm', help='force all the atom to be defined as HETATM type. The HETATM atom have CONECT defined by default.', action='store_true')
+    parser.add_argument('-i',
+                        '--inp',
+                        help='Input file. Can read multiple files and globbing',
+                        required=True,
+                        nargs='+')
+    parser.add_argument(
+        '-o',
+        '--out',
+        help=
+        'Output FORMAT (given as EXTENSION). The output filename will be the same filename with the given extension',
+        required=True)
+    parser.add_argument(
+        '--hetatm',
+        help='force all the atom to be defined as HETATM type. The HETATM atom have CONECT defined by default.',
+        action='store_true')
     parser.add_argument('--addh', help='Add hydrogens', action='store_true')
+    parser.add_argument(
+        '--retain_order',
+        help='The retain_order setting keeps atoms in the original order, instead of sorting them by atom identifiers',
+        action='store_true')
     args = parser.parse_args()
 
+    if args.retain_order:
+        cmd.set('retain_order', 'on')
     for i, inp in enumerate(args.inp):
         cmd.load(inp, f'inpmol{i}')
         if args.addh:
