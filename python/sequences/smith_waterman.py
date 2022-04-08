7928908f213af24264343335b9f639d481942118
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2022-04-08 15:55:09 +0200

    new file:   smith_waterman.py

---
+++ python/sequences/smith_waterman.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+# -*- coding: UTF8 -*-
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2022 Institut Pasteur                                       #
+#                 				                            #
+#                                                                           #
+#  Redistribution and use in source and binary forms, with or without       #
+#  modification, are permitted provided that the following conditions       #
+#  are met:                                                                 #
+#                                                                           #
+#  1. Redistributions of source code must retain the above copyright        #
+#  notice, this list of conditions and the following disclaimer.            #
+#  2. Redistributions in binary form must reproduce the above copyright     #
+#  notice, this list of conditions and the following disclaimer in the      #
+#  documentation and/or other materials provided with the distribution.     #
+#  3. Neither the name of the copyright holder nor the names of its         #
+#  contributors may be used to endorse or promote products derived from     #
+#  this software without specific prior written permission.                 #
+#                                                                           #
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
+#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
+#                                                                           #
+#  This program is free software: you can redistribute it and/or modify     #
+#                                                                           #
+#############################################################################
+
+# ref: https://tiefenauer.github.io/blog/smith-waterman/
+
+import itertools
+import numpy as np
+
+
+def matrix(a, b, match_score=3., gap_cost=2.):
+    """
+    >>> a = 'ggttgacta'
+    >>> len(a)
+    9
+    >>> b = 'tgttacgg'
+    >>> len(b)
+    8
+    >>> H = matrix(a, b)
+    >>> H.shape
+    (10, 9)
+    """
+    H = np.zeros((len(a) + 1, len(b) + 1))
+    for i, j in itertools.product(range(1, H.shape[0]), range(1, H.shape[1])):
+        match = H[i - 1, j - 1] + (match_score if a[i - 1] == b[j - 1] else -match_score)
+        delete = H[i - 1, j] - gap_cost
+        insert = H[i, j - 1] - gap_cost
+        H[i, j] = max(match, delete, insert, 0)
+    return H
+
+
+def traceback(H, b, b_='', old_i=0):
+    """
+    >>> a = 'ggttgacta'
+    >>> b = 'tgttacgg'
+    >>> H = matrix(a, b)
+    >>> traceback(H, b)
+    ('gtt-ac', 1)
+    """
+    # flip H to get index of **last** occurrence of H.max() with np.argmax()
+    H_flip = np.flip(np.flip(H, 0), 1)
+    i_, j_ = np.unravel_index(H_flip.argmax(), H_flip.shape)
+    i, j = np.subtract(H.shape, (i_ + 1, j_ + 1))  # (i, j) are **last** indexes of H.max()
+    if H[i, j] == 0:
+        return b_, j
+    b_ = b[j - 1] + '-' + b_ if old_i - i > 1 else b[j - 1] + b_
+    return traceback(H[0:i, 0:j], b, b_, i)
+
+
+def smith_waterman(a, b, match_score=3, gap_cost=2):
+    """
+    >>> a = 'ggttgacta'
+    >>> b = 'tgttacgg'
+    >>> smith_waterman(a, b)
+    (1, 7)
+    """
+    a, b = a.upper(), b.upper()
+    H = matrix(a, b, match_score, gap_cost)
+    b_, pos = traceback(H, b)
+    return pos, pos + len(b_)
+
+
+if __name__ == '__main__':
+    import sys
+    import doctest
+    import argparse
+    # ### UNCOMMENT FOR LOGGING ####
+    # import os
+    # import logging
+    # logfilename = os.path.splitext(os.path.basename(__file__))[0] + '.log'
+    # logging.basicConfig(filename=logfilename, level=logging.INFO, format='%(asctime)s: %(message)s')
+    # logging.info(f"################ Starting {__file__} ################")
+    # ### ##################### ####
+    # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
+    parser = argparse.ArgumentParser(description='')
+    # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
+    parser.add_argument('-a', '--arg1')
+    parser.add_argument('--test', help='Test the code', action='store_true')
+    args = parser.parse_args()
+
+    if args.test:
+        doctest.testmod(optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE)
+        sys.exit()
