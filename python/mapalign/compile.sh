869e8ef78913965ad1b30d94787d063db8f9b4d6
Guillaume Bouvier <bougui@ld18-1006.bis.pasteur.fr>
2022-04-14 09:15:22 +0200

    modified:   compile.sh

--- python/mapalign/compile.sh
+++ python/mapalign/compile.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 # -*- coding: UTF8 -*-
 
+[[ ! -d lib ]] && mkdir lib
 cc -fPIC -shared -o lib/initialize_matrix.so initialize_matrix.c
 cc -fPIC -shared -o lib/smith_waterman.so smith_waterman.c
