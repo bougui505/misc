7083eac8ae6bf99ca9c6597968aa3b580fb44766
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2022-05-16 15:42:04 +0200

    New network architecture 	modified:   nnet.py

--- python/protein/interpred/nnet.py
+++ python/protein/interpred/nnet.py
@@ -56,7 +56,7 @@ class InterPred(torch.nn.Module):
     >>> interpred = InterPred(verbose=True)
     >>> out = interpred(cmap_a, cmap_b, interseq)
     mat_in: torch.Size([1, 44, 224, 224])
-    out_fcn: torch.Size([1, 128, 54, 54])
+    out_fcn: torch.Size([1, 64, 54, 54])
     out_dense: torch.Size([1, 10000])
     out: torch.Size([1, 85, 13])
     """
@@ -75,7 +75,7 @@ class InterPred(torch.nn.Module):
             torch.nn.ReLU(),
             torch.nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, padding=1),
             torch.nn.ReLU(),
-            torch.nn.Conv2d(in_channels=384, out_channels=128, kernel_size=3, padding=1),
+            torch.nn.Conv2d(in_channels=384, out_channels=64, kernel_size=3, padding=1),
             torch.nn.ReLU(),
             # torch.nn.MaxPool2d(kernel_size=3, stride=2),
         ]
@@ -83,7 +83,7 @@ class InterPred(torch.nn.Module):
         # Fully connected of AlexNet
         layers = [
             torch.nn.Flatten(),
-            torch.nn.Linear(in_features=373248, out_features=10000),
+            torch.nn.Linear(in_features=186624, out_features=10000),
             torch.nn.ReLU(),
             torch.nn.Dropout(p=0.5),
             torch.nn.Linear(in_features=10000, out_features=10000),
