84212212aba0c999bee6852824230992624b0640
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2022-05-19 16:03:03 +0200

    modified:   filter_pdb.py

--- python/protein/filter_pdb.py
+++ python/protein/filter_pdb.py
@@ -56,15 +56,34 @@ def get_seq(pymolname, selection):
     return seq
 
 
-class Filter(object):
+class Properties(object):
     def __init__(self, pymolname):
         self.mol = pymolname
 
-    def n_chains(self, n=None, lower=None, upper=None):
+    def n_chains(self):
         chains = cmd.get_chains(f'{self.mol} and polymer.protein')
         n_chains = len(chains)
-        if n is not None:
-            return n_chains == n
+        return n_chains
+
+    def n_contacts(self):
+        chains = cmd.get_chains(f'{self.mol} and polymer.protein')
+        nchains = len(chains)
+        total = 0
+        if nchains > 1:
+            for i in range(nchains):
+                for j in range(i + 1, nchains):
+                    c1, c2 = chains[i], chains[j]
+                    _, ncontacts = self.isinteracting(c1, c2)
+                    total += ncontacts
+        return total
+
+    def nres(self):
+        chains = cmd.get_chains(f'{self.mol} and polymer.protein')
+        nres = []
+        for chain in chains:
+            coords = cmd.get_coords(f'{self.mol} and polymer.protein and name CA and chain {chain}')
+            nres.append(len(coords))
+        return nres
 
     def isinteracting(self, chain1, chain2, distance_thr=8.):
         coords1 = cmd.get_coords(f'{self.mol} and polymer.protein and name CA and chain {chain1}')
@@ -89,21 +108,6 @@ class Filter(object):
             seq += get_seq(self.mol, selection=f'polymer.protein and name CA and chain {chain}')
         return len(seq) == len(coords)
 
-    def isdimer(self):
-        """
-        2 chains in contact
-        """
-        chains = cmd.get_chains(f'{self.mol} and polymer.protein')
-        n_chains = len(chains)
-        if n_chains == 2:
-            isinter, ncontacts = self.isinteracting(chains[0], chains[1])
-            if ncontacts >= 10:
-                return isinter, n_chains, ncontacts
-            else:
-                return False, n_chains, ncontacts
-        else:
-            return False, n_chains, None
-
 
 class PDBdataset(torch.utils.data.Dataset):
     """
@@ -111,12 +115,9 @@ class PDBdataset(torch.utils.data.Dataset):
     See: ~/source/misc/shell/updatePDB.sh to download the PDB
 
     """
-    def __init__(self, pdbpath, selection='all', return_name=False, nchains=None, dimer=False):
+    def __init__(self, pdbpath, selection='all', return_name=False):
         self.list_IDs = glob.glob(f'{pdbpath}/**/*.ent.gz')
-        log('#file #nchains #n_contacts')
         self.return_name = return_name
-        self.nchains = nchains
-        self.dimer = dimer
         self.selection = selection
         cmd.reinitialize()
 
@@ -127,18 +128,13 @@ class PDBdataset(torch.utils.data.Dataset):
         pdbfile = self.list_IDs[index]
         pymolname = randomgen.randomstring()
         cmd.load(filename=pdbfile, object=pymolname)
-        pdbfilter = Filter(pymolname)
-        outfile = None
-        if self.nchains is not None:
-            if pdbfilter.n_chains(self.nchains):
-                outfile = pdbfile
-        if self.dimer is not None:
-            test, nchains, ncontacts = pdbfilter.isdimer()
-            if test:
-                if pdbfilter.checkseq():
-                    outfile = pdbfile
-        if outfile is not None:
-            log(f'{outfile} {nchains} {ncontacts}')
+        properties = Properties(pymolname)
+        seqOK = properties.checkseq()
+        n_chains = properties.n_chains()
+        ncontacts = properties.n_contacts()
+        nres = properties.nres()
+        nres_str = ','.join([f'{e:d}' for e in nres])
+        log(f'pdbfile: {pdbfile}|seqOK: {seqOK:d}|n_chains: {n_chains}|nres: {nres_str}|n_contacts: {ncontacts}')
         cmd.delete(pymolname)
 
 
@@ -159,8 +155,6 @@ if __name__ == '__main__':
     # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
     parser.add_argument('--test', help='Test the code', action='store_true')
     parser.add_argument('--pdb', help='Path to the pdb database')
-    parser.add_argument('--nchains', help='Retrieve pdb with this exact number of chains', type=int)
-    parser.add_argument('--dimer', help='Returns only dimers', action='store_true')
     args = parser.parse_args()
     # ### UNCOMMENT FOR LOGGING ####
     import os
@@ -173,7 +167,7 @@ if __name__ == '__main__':
     if args.test:
         doctest.testmod()
         sys.exit()
-    dataset = PDBdataset(args.pdb, nchains=args.nchains)
+    dataset = PDBdataset(args.pdb)
     dataloader = torch.utils.data.DataLoader(dataset,
                                              batch_size=1,
                                              shuffle=False,
