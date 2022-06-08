5381b2b902999b75a20b3ee07f0e4cfa53afe21a
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2022-06-08 09:41:47 +0200

    modified:   ../../../.gitignore 	new file:   PDBloader.py 	new file:   pdbfilelist.txt 	new file:   utils.py

---
+++ python/protein/sscl/PDBloader.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+# -*- coding: UTF8 -*-
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2021 Institut Pasteur                                       #
+#                 				                            #
+#                                                                           #
+#  Redistribution and use in source and binary forms, with or without       #
+#  modification, are permitted provided that the following conditions       #
+#  are met:                                                                 #
+#                                                                           #
+#  1. Redistributions of source code must retain the above copyright        #
+#  notice, this list of conditions and the following disclaimer.            #
+#  2. Redistributions in binary form must reproduce the above copyright     #
+#  notice, this list of conditions and the following disclaimer in the      #
+#  documentation and/or other materials provided with the distribution.     #
+#  3. Neither the name of the copyright holder nor the names of its         #
+#  contributors may be used to endorse or promote products derived from     #
+#  this software without specific prior written permission.                 #
+#                                                                           #
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
+#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
+#                                                                           #
+#  This program is free software: you can redistribute it and/or modify     #
+#                                                                           #
+#############################################################################
+
+import torch
+import glob
+from pymol import cmd
+from misc import randomgen
+from misc.pytorch import torchify
+import misc.protein.sscl.utils as utils
+import numpy as np
+
+
+def collate_fn(batch):
+    return batch
+
+
+class PDBdataset(torch.utils.data.Dataset):
+    """
+    Load pdb files from a PDB database and return coordinates
+    See: ~/source/misc/shell/updatePDB.sh to download the PDB
+
+    >>> # Generate random datapoints for testing
+    >>> dataset = PDBdataset('/media/bougui/scratch/pdb')
+    >>> dataloader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=False, num_workers=2, collate_fn=collate_fn)
+    >>> dataiter = iter(dataloader)
+    >>> for i, batch in enumerate(dataloader):
+    ...     print(len(batch))
+    ...     print([(dmat.shape, dmat_fragment.shape) for dmat, dmat_fragment in batch])
+    ...     if i == 0:
+    ...         break
+    4
+    [(torch.Size([1, 1, 249, 249]), torch.Size([1, 1, ..., ...])), (torch.Size([1, 1, 639, 639]), torch.Size([1, 1, ..., ...])), (torch.Size([1, 1, 390, 390]), torch.Size([1, 1, ..., ...])), (torch.Size([1, 1, 131, 131]), torch.Size([1, 1, ..., ...]))]
+
+
+    >>> dataset = PDBdataset(pdblistfile='pdbfilelist.txt', return_name=True)
+    >>> dataloader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=False, num_workers=2, collate_fn=collate_fn)
+    >>> dataiter = iter(dataloader)
+    >>> batch = next(dataiter)
+    >>> len(batch)
+    4
+    >>> [(dmat.shape, dmat_fragment.shape, name) for dmat, dmat_fragment, name in batch]
+    [(torch.Size([1, 1, 162, 162]), torch.Size([1, 1, ..., ...]), 'pdb/00/pdb200l.ent.gz_A'), (torch.Size([1, 1, 154, 154]), torch.Size([1, 1, ..., ...]), 'pdb/01/pdb101m.ent.gz_A'), (torch.Size([1, 1, 154, 154]), torch.Size([1, 1, ..., ...]), 'pdb/02/pdb102m.ent.gz_A'), (torch.Size([1, 1, 163, 163]), torch.Size([1, 1, ..., ...]), 'pdb/02/pdb102l.ent.gz_A')]
+    """
+    def __init__(
+            self,
+            pdbpath=None,
+            pdblist=None,
+            pdblistfile=None,  # format: pdbfilename chain
+            selection='polymer.protein and name CA',
+            return_name=False):
+        self.chain_from_list = False
+        self.return_name = return_name
+        if pdbpath is not None:
+            self.list_IDs = glob.glob(f'{pdbpath}/**/*.ent.gz')
+        if pdblist is not None:
+            self.list_IDs = pdblist
+        if pdblistfile is not None:
+            with open(pdblistfile, 'r') as filelist:
+                self.list_IDs = filelist.read().splitlines()
+            self.chain_from_list = True
+        self.selection = selection
+        cmd.reinitialize()
+
+    def __len__(self):
+        return len(self.list_IDs)
+
+    def __getitem__(self, index):
+        if not self.chain_from_list:
+            pdbfile = self.list_IDs[index]
+        else:
+            pdbfile, chain = self.list_IDs[index].split()
+        pymolname = randomgen.randomstring()
+        cmd.load(filename=pdbfile, object=pymolname)
+        chains = cmd.get_chains(f'{pymolname} and {self.selection}')
+        nchains = len(chains)
+        if nchains == 0:
+            if self.return_name:
+                return None, pdbfile
+            else:
+                return None
+        else:
+            if not self.chain_from_list:
+                chain = np.random.choice(chains)
+            coords = cmd.get_coords(selection=f'{pymolname} and {self.selection} and chain {chain}')
+            if coords is None:
+                if self.return_name:
+                    return None, pdbfile + "_" + chain
+                else:
+                    return None
+            coords = torch.tensor(coords[None, ...])
+            fragment = utils.get_random_fragment(coords)
+            dmat = utils.get_dmat(coords)
+            dmat_fragment = utils.get_dmat(fragment)
+        cmd.delete(pymolname)
+        if self.return_name:
+            return dmat, dmat_fragment, pdbfile + "_" + chain
+        else:
+            return dmat, dmat_fragment
+
+
+if __name__ == '__main__':
+    import sys
+    import argparse
+    import doctest
+    # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
+    parser = argparse.ArgumentParser(description='')
+    # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
+    parser.add_argument('--test', help='Test the code', action='store_true')
+    args = parser.parse_args()
+
+    if args.test:
+        doctest.testmod(optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE)
+        sys.exit()
---
+++ python/protein/sscl/utils.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python3
+# -*- coding: UTF8 -*-
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2022 Institut Pasteur                                       #
+#                 				                            #
+#                                                                           #
+#  Redistribution and use in source and binary forms, with or without       #
+#  modification, are permitted provided that the following conditions       #
+#  are met:                                                                 #
+#                                                                           #
+#  1. Redistributions of source code must retain the above copyright        #
+#  notice, this list of conditions and the following disclaimer.            #
+#  2. Redistributions in binary form must reproduce the above copyright     #
+#  notice, this list of conditions and the following disclaimer in the      #
+#  documentation and/or other materials provided with the distribution.     #
+#  3. Neither the name of the copyright holder nor the names of its         #
+#  contributors may be used to endorse or promote products derived from     #
+#  this software without specific prior written permission.                 #
+#                                                                           #
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
+#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
+#                                                                           #
+#  This program is free software: you can redistribute it and/or modify     #
+#                                                                           #
+#############################################################################
+
+import torch
+from misc import randomgen
+from pymol import cmd
+import numpy as np
+import itertools
+
+
+def get_dmat(coords):
+    """
+    >>> coords = torch.randn(1, 10, 3)
+    >>> dmat = get_dmat(coords)
+    >>> dmat.shape
+    torch.Size([1, 1, 10, 10])
+    """
+    dmat = torch.cdist(coords, coords)
+    dmat = dmat[:, None, ...]  # Add the channel dimension
+    return dmat
+
+
+def get_random_fragment(coords):
+    """
+    >>> coords = torch.randn(1, 100, 3)
+    >>> fragment = get_random_fragment(coords)
+    >>> fragment.shape
+    torch.Size([1, ..., 3])
+    """
+    fragment_ratio = np.random.uniform()
+    n = coords.shape[1]
+    fragment_size = int(fragment_ratio * n)
+    i_start_max = n - fragment_size
+    i_start = np.random.choice(i_start_max)
+    i_stop = i_start + fragment_size
+    inds = np.r_[i_start:i_stop]
+    fragment = coords[:, inds, :]
+    return fragment
+
+
+class Normalizer(object):
+    def __init__(self, batch):
+        """
+        >>> batch = [1 + torch.randn(1, 1, 249, 249), 2 + 2* torch.randn(1, 1, 639, 639), 3 + 3 * torch.randn(1, 1, 390, 390), 4 + 4 * torch.randn(1, 1, 131, 131)]
+        >>> normalizer = Normalizer(batch)
+        >>> [torch.round(e) for e in normalizer.mu]
+        [tensor(1.), tensor(2.), tensor(3.), tensor(4.)]
+        >>> [torch.round(e) for e in normalizer.sigma]
+        [tensor(1.), tensor(2.), tensor(3.), tensor(4.)]
+        >>> out = normalizer.transform(batch)
+        >>> [torch.round(e.mean()).abs() for e in out]
+        [tensor(0.), tensor(0.), tensor(0.), tensor(0.)]
+        >>> [torch.round(e.std()) for e in out]
+        [tensor(1.), tensor(1.), tensor(1.), tensor(1.)]
+        >>> x = normalizer.inverse_transform(out)
+        >>> [torch.round(e.mean()) for e in x]
+        [tensor(1.), tensor(2.), tensor(3.), tensor(4.)]
+        >>> [torch.round(e.std()) for e in x]
+        [tensor(1.), tensor(2.), tensor(3.), tensor(4.)]
+        """
+        self.batch = [e for e in batch if e is not None]
+        self.mu = torch.tensor([e.mean() for e in self.batch])
+        self.sigma = torch.tensor([e.std() for e in self.batch])
+
+    def transform(self, x):
+        n = len(x)
+        out = []
+        for i in range(n):
+            if self.sigma[i] > 0:
+                out.append((x[i] - self.mu[i]) / self.sigma[i])
+            else:
+                out.append(x[i] - self.mu[i])
+        return out
+
+    def inverse_transform(self, x):
+        n = len(x)
+        out = []
+        for i in range(n):
+            out.append(x[i] * self.sigma[i] + self.mu[i])
+        return out
+
+
+def compute_pad(inp_size, out_size):
+    """
+    >>> A = torch.randn(78, 83)
+    >>> out_size = (224, 224)
+    >>> compute_pad(A.shape, out_size)
+    (70, 71, 73, 73)
+    """
+    na, nb = inp_size
+    nat, nbt = out_size
+    assert na <= nat
+    assert nb <= nbt
+    narest, nbrest = nat - na, nbt - nb
+    padtop = narest // 2
+    padbottom = narest - padtop
+    padleft = nbrest // 2
+    padright = nbrest - padleft
+    return (padleft, padright, padtop, padbottom)
+
+
+def pad(mat, size):
+    """
+    >>> A = torch.randn(3, 1, 78, 83)
+    >>> B = pad(A, size=(224, 224))
+    >>> B.shape
+    torch.Size([3, 1, 224, 224])
+    """
+    padlen = compute_pad(mat.shape[-2:], size)
+    return torch.nn.functional.pad(mat, padlen)
+
+
+def unpad(mat, size):
+    """
+    >>> A = torch.randn(3, 1, 78, 83)
+    >>> B = pad(A, size=(224, 224))
+    >>> B.shape
+    torch.Size([3, 1, 224, 224])
+    >>> A2 = unpad(B, (78, 83))
+    >>> A2.shape
+    torch.Size([3, 1, 78, 83])
+    >>> (A2 == A).all()
+    tensor(True)
+    """
+    na, nb = mat.shape[-2:]
+    padlen = compute_pad(size, (na, nb))
+    padleft, padright, padtop, padbottom = padlen
+    return mat[..., padtop:na - padbottom, padleft:nb - padright]
+
+
+def resize(mat, size):
+    """
+    >>> A = torch.randn(3, 1, 78, 78)
+    >>> size = (224, 224)
+    >>> B = resize(A, size=size)
+    >>> B.shape
+    torch.Size([3, 1, 224, 224])
+
+    >>> A = torch.randn(3, 1, 278, 278)
+    >>> B = resize(A, size=size)
+    >>> B.shape
+    torch.Size([3, 1, 224, 224])
+    """
+    na, nb = mat.shape[-2:]
+    nat, nbt = size
+    if na < nat and nb < nbt:
+        out = pad(mat, size)
+    elif na > nat and nb > nbt:
+        out = torch.nn.functional.interpolate(mat, size=size)
+    else:
+        out = mat
+    return out
+
+
+def back_transform(mat, size):
+    """
+    >>> A = torch.randn(3, 1, 78, 78)
+    >>> A_resize = resize(A, (224, 224))  # Here padding is applied
+    >>> A_resize.shape
+    torch.Size([3, 1, 224, 224])
+    >>> A_back = back_transform(A_resize, (78, 78))  # and unpadding
+    >>> A_back.shape
+    torch.Size([3, 1, 78, 78])
+    >>> (A_back == A).all()  # Therefore we retrieve the same A matrix
+    tensor(True)
+
+    >>> A = torch.randn(3, 1, 378, 378)
+    >>> A_resize = resize(A, (224, 224))  # Here interpolation is applied to reduce the size
+    >>> A_resize.shape
+    torch.Size([3, 1, 224, 224])
+    >>> A_back = back_transform(A_resize, (378, 378))  # and back interpolation
+    >>> A_back.shape
+    torch.Size([3, 1, 378, 378])
+    >>> (A_back == A).all()  # therefore some information is lost
+    tensor(False)
+    """
+    na, nb = mat.shape[-2:]
+    nat, nbt = size
+    if nat < na and nbt < nb:
+        out = unpad(mat, size)
+    elif nat > na and nbt > nb:
+        out = torch.nn.functional.interpolate(mat, size=size)
+    else:
+        out = mat
+    return out
+
+
+def log(msg):
+    try:
+        logging.info(msg)
+    except NameError:
+        pass
+
+
+if __name__ == '__main__':
+    import sys
+    import doctest
+    import argparse
+    import matplotlib.pyplot as plt
+    ### UNCOMMENT FOR LOGGING ####
+    import os
+    import logging
+    logfilename = os.path.splitext(os.path.basename(__file__))[0] + '.log'
+    logging.basicConfig(filename=logfilename, level=logging.INFO, format='%(asctime)s: %(message)s')
+    logging.info(f"################ Starting {__file__} ################")
+    ### ##################### ####
+    # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
+    parser = argparse.ArgumentParser(description='')
+    # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
+    parser.add_argument('-a', '--arg1')
+    parser.add_argument('--test', help='Test the code', action='store_true')
+    args = parser.parse_args()
+
+    if args.test:
+        doctest.testmod(optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE)
+        sys.exit()
