768e4bcab0b0b94d175b5a23e1ca8b541b06e26a
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2022-08-25 14:35:08 +0200

    new file:   argos/README 	new file:   argos/argos.sh 	new file:   argos/rclone.3s+.sh 	new file:   argos/top.3s+.sh 	new file:   argos/weather.15m.sh

---
+++ shell/argos/rclone.3s+.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+# -*- coding: UTF8 -*-
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2022 Institut Pasteur                                       #
+#                 				                            #
+#                                                                           #
+#  Redistribution and use in source and binary forms, with or without       #
+#  modification, are permitted provided that the following conditions       #
+#  are met:                                                                 #    
+#                                                                           #
+#  1. Redistributions of source code must retain the above copyright        #
+#  notice, this list of conditions and the following disclaimer.            #
+#  2. Redistributions in binary form must reproduce the above copyright     #
+#  notice, this list of conditions and the following disclaimer in the      #
+#  documentation and/or other materials provided with the distribution.     #
+#  3. Neither the name of the copyright holder nor the names of its         #
+#  contributors may be used to endorse or promote products derived from     #
+#  this software without specific prior written permission.                 #
+#                                                                           #
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
+#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
+#                                                                           #
+#  This program is free software: you can redistribute it and/or modify     #
+#                                                                           #
+#############################################################################
+
+set -e  # exit on error
+set -o pipefail  # exit when a process in the pipe fails
+set -o noclobber  # prevent overwritting redirection
+
+# Full path to the directory of the current script
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+
+function backup_delay() { # Backup delay before starting
+    # One backup every 6h
+    DELTAT=$(echo "6*60*60-$(date +%s)+$(cat $INSTALLDIR/.rclone_timestamp.txt)" | bc)
+    date -d@$DELTAT -u +%H:%M:%S
+}
+
+function rclone_info () {
+    pgrep -a rclone > /dev/null \
+        && echo "🔄: $(ps -p $(pgrep -f "rclone sync") -o etime | tail -1 | tr -d ' ')" \
+        || echo "🔄⏳: $(backup_delay)"
+}
+
+INSTALLDIR="/home/bougui/source/rclone_GoogleDrive_backup"
+
+rclone_info
+echo "---"
+
+if [ "$ARGOS_MENU_OPEN" == "true" ]; then
+    tail $INSTALLDIR/rclone.log
+else
+    echo "Loading..."
+fi
