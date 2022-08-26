2eb2d5e52ee6c4c8ec28ba7f6c39cf399a64fe4b
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2022-08-26 16:40:37 +0200

    resample argument 	modified:   mrc.py

--- python/Grid3/mrc.py
+++ python/Grid3/mrc.py
@@ -48,14 +48,10 @@ import os
 from misc.protein.rotate import rotate as rotate_coords
 from misc.protein.rotate import get_rotation_matrix
 from misc.protein.rotate import rotate_pdb
+import scipy.ndimage
 
 
-def save_density(density,
-                 outfilename,
-                 spacing=1,
-                 origin=[0, 0, 0],
-                 padding=0,
-                 transpose=True):
+def save_density(density, outfilename, spacing=1, origin=[0, 0, 0], padding=0, transpose=True):
     """
     Save the density file as mrc for the given atomname
     """
@@ -143,12 +139,7 @@ def digitize(coords, data, origin, spacing):
     return inds
 
 
-def mrc_to_pdb(mrcfilename,
-               outpdb,
-               minthr=-np.inf,
-               maxthr=np.inf,
-               stride=1,
-               normalize=False):
+def mrc_to_pdb(mrcfilename, outpdb, minthr=-np.inf, maxthr=np.inf, stride=1, normalize=False):
     """
     Create a pdb file from the given mrcfilename
     """
@@ -182,23 +173,15 @@ def translate_pdb(pdbfilename, outpdbfilename, translation):
     cmd.save(outpdbfilename, selection='inpdb')
 
 
-def rotate(grid,
-           rotation_matrix=None,
-           angle_x=0,
-           angle_y=0,
-           angle_z=0,
-           center=None):
+def rotate(grid, rotation_matrix=None, angle_x=0, angle_y=0, angle_z=0, center=None):
     n, p, q = grid.shape
     outgrid = np.zeros_like(grid)
     counts = np.zeros_like(grid)
-    coords, values = filter_by_condition(grid, np.ones_like(grid,
-                                                            dtype=np.bool))
+    coords, values = filter_by_condition(grid, np.ones_like(grid, dtype=np.bool))
     coords = np.float_(coords)
     if rotation_matrix is None:
         rotation_matrix = get_rotation_matrix(angle_x, angle_y, angle_z)
-    coords_new = rotate_coords(coords,
-                               rotation_matrix=rotation_matrix,
-                               center=center)
+    coords_new = rotate_coords(coords, rotation_matrix=rotation_matrix, center=center)
     coords_new = np.int_(np.round(coords_new))
     sel = np.all(coords_new > 0, axis=1)
     sel = sel & (coords_new[:, 0] < n)
@@ -208,79 +191,67 @@ def rotate(grid,
     values = values[sel]
     np.add.at(counts, tuple(coords_new.T), 1.)
     np.add.at(outgrid, tuple(coords_new.T), values)
-    outgrid = np.divide(outgrid,
-                        counts,
-                        where=(counts != 0),
-                        out=np.zeros_like(outgrid))
+    outgrid = np.divide(outgrid, counts, where=(counts != 0), out=np.zeros_like(outgrid))
     return outgrid
 
 
+def resample(grid, spacing, zoom):
+    """
+    >>> grid = np.random.uniform(low=0., high=1., size=(10, 20, 30))
+    >>> spacing = 1.
+    >>> out, spacing_out = resample(grid, spacing, 2)
+    >>> out.shape
+    (20, 40, 60)
+    >>> spacing_out
+    0.5
+    """
+    out = scipy.ndimage.zoom(grid, zoom=zoom)
+    spacing_out = spacing / zoom
+    return out, spacing_out
+
+
 if __name__ == '__main__':
+    import doctest
     import argparse
     # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
     parser = argparse.ArgumentParser(description='')
     # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
-    parser.add_argument(
-        '--npy', help='Read a npy file and create a MRC file (see: --out)')
-    parser.add_argument('--origin',
-                        type=float,
-                        nargs='+',
-                        default=None,
-                        help='Origin for the MRC file')  # 10. 20. 30.
-    parser.add_argument('--center',
-                        help='Set the origin to 0 0 0',
-                        action='store_true')
-    parser.add_argument('--rotate',
-                        help='x, y, z angles of rotation in degree',
-                        nargs='+',
+    parser.add_argument('--npy', help='Read a npy file and create a MRC file (see: --out)')
+    parser.add_argument('--origin', type=float, nargs='+', default=None, help='Origin for the MRC file')  # 10. 20. 30.
+    parser.add_argument('--center', help='Set the origin to 0 0 0', action='store_true')
+    parser.add_argument('--resample',
+                        help='Resample the grid by the given ratio. >1: bigger grid. <1: smaller grid',
                         type=float)
+    parser.add_argument('--rotate', help='x, y, z angles of rotation in degree', nargs='+', type=float)
     parser.add_argument(
         '--pdb',
-        help=
-        'PDB file to translate when center option is called in order to align the pdb and the centered grid'
-    )
-    parser.add_argument('--spacing',
-                        type=float,
+        help='PDB file to translate when center option is called in order to align the pdb and the centered grid')
+    parser.add_argument('--spacing', type=float, default=1, help='Spacing for the MRC file')  # 1.
+    parser.add_argument('--out', help='Out MRC file name (see: --npy)')  # 10. 20. 30.
+    parser.add_argument('--mrc', help='Read the given MRC and print the flatten data to stdout if --outpdb not given')
+    parser.add_argument('--outpdb', help='Convert the given mrc file (--mrc) to pdb')
+    parser.add_argument('--outnpy', help='Convert the given mrc file (--mrc) to npy file')
+    parser.add_argument('--minthr',
+                        help='Minimum threshold the MRC to save to pdb (--outpdb)',
+                        default=-np.inf,
+                        type=float)
+    parser.add_argument('--maxthr',
+                        help='Maximum threshold the MRC to save to pdb (--outpdb)',
+                        default=np.inf,
+                        type=float)
+    parser.add_argument('--stride',
+                        help='Stride for the grid to save to pdb (--outpdb), default=1',
                         default=1,
-                        help='Spacing for the MRC file')  # 1.
-    parser.add_argument('--out',
-                        help='Out MRC file name (see: --npy)')  # 10. 20. 30.
-    parser.add_argument(
-        '--mrc',
-        help=
-        'Read the given MRC and print the flatten data to stdout if --outpdb not given'
-    )
-    parser.add_argument('--outpdb',
-                        help='Convert the given mrc file (--mrc) to pdb')
-    parser.add_argument('--outnpy',
-                        help='Convert the given mrc file (--mrc) to npy file')
-    parser.add_argument(
-        '--minthr',
-        help='Minimum threshold the MRC to save to pdb (--outpdb)',
-        default=-np.inf,
-        type=float)
-    parser.add_argument(
-        '--maxthr',
-        help='Maximum threshold the MRC to save to pdb (--outpdb)',
-        default=np.inf,
-        type=float)
-    parser.add_argument(
-        '--stride',
-        help='Stride for the grid to save to pdb (--outpdb), default=1',
-        default=1,
-        type=int)
-    parser.add_argument('--normalize',
-                        help='Normalize the density between 0 and 1',
-                        action='store_true')
-    parser.add_argument(
-        '--info',
-        help='Print informations about the given mrc (see --mrc)',
-        action='store_true')
-    parser.add_argument('--padding',
-                        help='Add a padding to the given map',
-                        type=int,
-                        default=0)
+                        type=int)
+    parser.add_argument('--normalize', help='Normalize the density between 0 and 1', action='store_true')
+    parser.add_argument('--info', help='Print informations about the given mrc (see --mrc)', action='store_true')
+    parser.add_argument('--padding', help='Add a padding to the given map', type=int, default=0)
+    parser.add_argument('--test', help='Test the code', action='store_true')
     args = parser.parse_args()
+
+    if args.test:
+        doctest.testmod(optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE)
+        sys.exit()
 
     if args.pdb is not None:
         cmd.load(args.pdb, object='pdbin')
@@ -288,9 +259,9 @@ if __name__ == '__main__':
         data = np.load(args.npy)
         transpose = True
     if args.mrc is not None:
-        data, origin_in, args.spacing = mrc_to_array(args.mrc,
-                                                     normalize=args.normalize,
-                                                     padding=args.padding)
+        data, origin_in, args.spacing = mrc_to_array(args.mrc, normalize=args.normalize, padding=args.padding)
+        if args.resample is not None:
+            data, args.spacing = resample(data, args.spacing, args.resample)
         if args.center:
             args.origin = np.asarray([0, 0, 0])
             translation = -origin_in
@@ -302,11 +273,7 @@ if __name__ == '__main__':
             data = rotate(data, angle_x=-gamma, angle_y=beta, angle_z=-alpha)
             if args.pdb is not None:
                 outpdbname = f'{os.path.splitext(args.out)[0]}.pdb'
-                rotate_pdb(args.pdb,
-                           outpdbname,
-                           angle_x=alpha,
-                           angle_y=beta,
-                           angle_z=gamma)
+                rotate_pdb(args.pdb, outpdbname, angle_x=alpha, angle_y=beta, angle_z=gamma)
         if args.origin is None:
             args.origin = origin_in
         transpose = False
@@ -330,9 +297,4 @@ if __name__ == '__main__':
     if args.outnpy is not None:
         np.save(args.outnpy, data)
     if args.out is not None:
-        save_density(data,
-                     args.out,
-                     args.spacing,
-                     args.origin,
-                     0,
-                     transpose=transpose)
+        save_density(data, args.out, args.spacing, args.origin, 0, transpose=transpose)
