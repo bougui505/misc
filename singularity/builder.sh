b7374e7024b25c3d8bd2b494d392cc0912d74642
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2022-10-04 10:09:16 +0200

    new file:   builder.sh 	modified:   pytorch.def

---
+++ singularity/builder.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env zsh
+# -*- coding: UTF8 -*-
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2022 Institut Pasteur                                       #
+#                 				                            #
+#                                                                           #
+#  Redistribution and use in source and binary forms, with or without       #
+#  modification, are permitted provided that the following conditions       #
+#  are met:                                                                 #    
+#                                                                           #
+#  1. Redistributions of source code must retain the above copyright        #
+#  notice, this list of conditions and the following disclaimer.            #
+#  2. Redistributions in binary form must reproduce the above copyright     #
+#  notice, this list of conditions and the following disclaimer in the      #
+#  documentation and/or other materials provided with the distribution.     #
+#  3. Neither the name of the copyright holder nor the names of its         #
+#  contributors may be used to endorse or promote products derived from     #
+#  this software without specific prior written permission.                 #
+#                                                                           #
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
+#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
+#                                                                           #
+#  This program is free software: you can redistribute it and/or modify     #
+#                                                                           #
+#############################################################################
+
+set -e  # exit on error
+set -o pipefail  # exit when a process in the pipe fails
+set -o noclobber  # prevent overwritting redirection
+
+# Full path to the directory of the current script
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+
+function usage () {
+    cat << EOF
+Help message
+    -h, --help print this help message and exit
+    -d, --def def file to build
+    -f, --force The --force option will delete and overwrite an existing Singularity image without presenting the normal interactive prompt
+EOF
+}
+
+DEF=1  # Default value
+FORCE=0
+while [ "$#" -gt 0 ]; do
+    case $1 in
+        -d|--def) DEF="$2"; shift ;;
+        -f|--force) FORCE=1;;
+        -h|--help) usage; exit 0 ;;
+        --) OTHER="${@:2}";break; shift;;  # Everything after the '--' symbol
+        *) usage; exit 1 ;;
+    esac
+    shift
+done
+
+BUILDCMD="sudo singularity build"
+if [ $FORCE -eq 1 ]; then
+    BUILDCMD="$BUILDCMD --force"
+fi
+
+case $DEF in
+    debian.def|pymol.def)
+        eval "$BUILDCMD debian.sif debian.def";;
+    pymol.def|pytorch.def)
+        eval "$BUILDCMD pymol.sif pymol.def";;
+    pytorch.def)
+        eval "$BUILDCMD pytorch.sif pytorch.def";;
+esac
