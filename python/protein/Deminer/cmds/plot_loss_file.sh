1e4cc8a4d88897e44041986733253258713994b9
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2022-10-06 17:16:53 +0200

    modified:   cmds/plot_loss_file.sh

--- python/protein/Deminer/cmds/plot_loss_file.sh
+++ python/protein/Deminer/cmds/plot_loss_file.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env zsh
-rsync desk:/ld18-1006/work/bougui/Deminer/logs . && LASTUPD=$(grep ': epoch:' logs/deminer.log | tail -1 | awk '{print $1,$2}') && awkfields -F 'loss' -f logs/deminer.log | plot --xlabel 'step' --ylabel 'loss'  --ws 1000 --ymax 2.8 --grid --title $LASTUPD --save /home/bougui/Documents/presentations/20220929_DeMiner/figs/loss.svg
+rsync desk:/ld18-1006/work/bougui/Deminer/logs . &&
+LASTUPD=$(grep ': epoch:' logs/deminer.log | tail -1 | awk '{print $1,$2}') &&
+awkfields -F 'loss' -f logs/deminer.log | plot --xlabel 'step' --ylabel 'loss'  --ws 1000 --ymax 2.8 --grid --title $LASTUPD --save /home/bougui/Documents/presentations/20220929_DeMiner/figs/loss.svg
