2fd4c6e32314441a59c1f235fd2f761b4cfffa10
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2022-10-10 16:59:52 +0200

    modified:   NNindex.py 	modified:   README.md

--- python/annoy/NNindex.py
+++ python/annoy/NNindex.py
@@ -35,6 +35,19 @@
 #  This program is free software: you can redistribute it and/or modify     #
 #                                                                           #
 #############################################################################
+"""
+Helper class to generate an Annoy index for fast Approximate Nearest neighbors search.
+See: https://github.com/spotify/annoy
+
+>>> np.random.seed(0)
+>>> nnindex = NNindex(128)
+>>> for name in ['a', 'b', 'c', 'd', 'e', 'f', 'g']:
+...     nnindex.add(np.random.normal(size=(128)), name)
+>>> nnindex.build(10)
+>>> nnindex.query('c', k=3)
+(['c', 'g', 'e'], [0.0, 14.265301704406738, 14.367243766784668])
+"""
+
 import os
 from annoy import AnnoyIndex
 import h5py
