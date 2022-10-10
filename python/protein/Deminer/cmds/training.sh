0d96ba201eeb361682855ed9c4aca8bdbf079a39
Guillaume Bouvier <bougui@ld18-1006.bis.pasteur.fr>
2022-10-10 11:54:59 +0200

    modified:   cmds/training.sh

--- python/protein/Deminer/cmds/training.sh
+++ python/protein/Deminer/cmds/training.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env zsh
-run --nv -B /ld18-1006 -- ./deminer.py --train --print_each 1 --n_epochs 5
+until run --nv -B /ld18-1006 -- ./deminer.py --train --print_each 1 --n_epochs 5; do echo "Auto restart"; sleep 60; done
