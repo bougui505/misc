9357738a22886fa9732db423d1feb4b1cc226a58
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2022-10-10 13:49:41 +0200

    modified:   cmds/update_model.sh

--- python/protein/Deminer/cmds/update_model.sh
+++ python/protein/Deminer/cmds/update_model.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env zsh
-rsync desk:/ld18-1006/work/bougui/Deminer/model.pt 20221005_model.pt
+cd models && bk 20221005_model.pt && rsync desk:/ld18-1006/work/bougui/Deminer/model.pt 20221005_model.pt && cd -
