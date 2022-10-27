4df1f3c5d5845fd33345c2932402dd222a4f7285
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2022-10-27 11:36:42 +0200

    modified:   cmds/build-extra-shell.sh 	modified:   extra.def 	modified:   shell.def

--- singularity/conda/cmds/build-extra-shell.sh
+++ singularity/conda/cmds/build-extra-shell.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env zsh
-sudo singularity build --force --sandbox extra.{sif,def} && sudo singularity build --force --sandbox shell.{sif,def} && sudo rsync -a -zz --update --info=progress2 -h --delete -e 'ssh -F /home/bougui/.ssh/config' shell.sif desk:~/source/misc/singularity/conda
+sudo singularity build --force --sandbox extra.{sif,def} && sudo rsync -a -zz --update --info=progress2 -h --delete -e 'ssh -F /home/bougui/.ssh/config' extra.sif desk:~/source/misc/singularity/conda
