d6db406a24c55ebd1b145d0af8991372687c7f49
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-01-30 10:16:26 +0100

    new file:   catcol.sh

---
+++ shell/catcol.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env zsh
+# -*- coding: UTF8 -*-
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2023 Institut Pasteur                                       #
+#                 				                            #
+#                                                                           #
+#  Redistribution and use in source and binary forms, with or without       #
+#  modification, are permitted provided that the following conditions       #
+#  are met:                                                                 #    
+#                                                                           #
+#  1. Redistributions of source code must retain the above copyright        #
+#  notice, this list of conditions and the following disclaimer.            #
+#  2. Redistributions in binary form must reproduce the above copyright     #
+#  notice, this list of conditions and the following disclaimer in the      #
+#  documentation and/or other materials provided with the distribution.     #
+#  3. Neither the name of the copyright holder nor the names of its         #
+#  contributors may be used to endorse or promote products derived from     #
+#  this software without specific prior written permission.                 #
+#                                                                           #
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
+#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
+#                                                                           #
+#  This program is free software: you can redistribute it and/or modify     #
+#                                                                           #
+#############################################################################
+
+set -e  # exit on error
+set -o pipefail  # exit when a process in the pipe fails
+# set -o noclobber  # prevent overwritting redirection
+
+# Full path to the directory of the current script
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+
+function usage () {
+    cat << EOF
+Help message
+    -h, --help print this help message and exit
+EOF
+}
+
+DELIMITER=" "  # Default value
+while [ "$#" -gt 0 ]; do
+    case $1 in
+        -d|--delimiter) DELIMITER="$2"; shift ;;
+        -h|--help) usage; exit 0 ;;
+        --) OTHER="${@:2}";break; shift;;  # Everything after the '--' symbol
+        *) usage; exit 1 ;;
+    esac
+    shift
+done
+
+awk -F"$DELIMITER" '{
+    for (i=1; i<=NF; i++){
+        DATA[ARGIND][FNR][i] = $i
+    }
+}
+END{
+    for (i in DATA){  # FILES
+        for (j in DATA[i]){ # ROWS
+            for (k in DATA[i][j]){ # COLUMNS
+                ROW=j
+                COL=k+k_prev
+                VAL=DATA[i][j][k]
+                OUT[ROW][COL] = VAL
+            }
+        }
+        k_prev = COL
+    }
+    for (ROW in OUT){
+        COLMAX=0
+        for (COL in OUT[ROW]){
+            if (COL>COLMAX){
+                COLMAX=COL
+            }
+        }
+        for (COL=1;COL<=COLMAX;COL++){
+            if (COL<COLMAX){
+                printf OUT[ROW][COL]","
+            }
+            else{
+                printf OUT[ROW][COL]
+            }
+        }
+        printf "\n"
+    }
+}
+' $(echo $OTHER)
