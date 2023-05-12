c09e5d50641b17e6b777257c7e784afa1fbc94ca
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-05-12 09:01:58 +0200

    modified:   proteingraph.py

--- python/protein/graph/proteingraph.py
+++ python/protein/graph/proteingraph.py
@@ -40,11 +40,34 @@ import pymol2
 import numpy as np
 import torch
 import scipy.spatial.distance as scidist
+import logging
+if not os.path.isdir('logs'):
+    os.mkdir('logs')
+logfilename = 'logs/' + os.path.splitext(os.path.basename(__file__))[0] + '.log'
+logging.basicConfig(filename=logfilename, level=logging.INFO, format='%(asctime)s: %(message)s')
+logging.info(f"################ Starting {__file__} ################")
 
 AMINO_ACIDS = [
     'ALA', 'ARG', 'ASN', 'ASP', 'CYS', 'GLN', 'GLU', 'GLY', 'HIS', 'ILE', 'LEU', 'LYS', 'MET', 'PHE', 'PRO', 'SER',
     'THR', 'TRP', 'TYR', 'VAL', 'XXX'
 ]
+
+ATOMS = [
+    'CE3', 'CG1', 'ND1', 'N', 'NE1', 'CG2', 'ND2', 'CZ', 'O', 'CH2', 'NE2', 'C', 'CA', 'CB', 'NH1', 'NE', 'OD1', 'NH2',
+    'CD', 'CE', 'OE1', 'OD2', 'CZ2', 'OG', 'OE2', 'OH', 'CZ3', 'OG1', 'SD', 'CG', 'CD1', 'CE1', 'CD2', 'SG', 'CE2',
+    'NZ', 'XXX'
+]
+
+
+def getclasslist(mapping, inplist):
+    classlist = []
+    for e in inplist:
+        if e in mapping:
+            classlist.append(mapping[e])
+        else:
+            classlist.append(mapping['XXX'])
+            log(f"unknown key {e}")
+    return torch.tensor(classlist)
 
 
 def seq_to_1hot(seqlist):
@@ -52,12 +75,38 @@ def seq_to_1hot(seqlist):
     >>> seqlist = ['GLY', 'SER', 'GLN', 'ILE', 'PRO', 'ALA', 'SER', 'GLU', 'GLN', 'GLU', 'DLY', 'THR', 'LEU']
     >>> len(seqlist)
     13
-    >>> seq_to_1hot(seqlist).shape
+    >>> onehot = seq_to_1hot(seqlist)
+    >>> onehot.shape
     torch.Size([13, 21])
+    >>> onehot[0]
+    tensor([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    >>> onehot[10]
+    tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
     """
     mapping = dict(zip(AMINO_ACIDS, range(len(AMINO_ACIDS))))
-    classlist = torch.tensor([mapping[e] if e in mapping else mapping['XXX'] for e in seqlist])
+    classlist = getclasslist(mapping, seqlist)
     onehot = torch.nn.functional.one_hot(classlist, num_classes=len(AMINO_ACIDS))
+    return onehot
+
+
+def atomlist_to_1hot(atomlist):
+    """
+    >>> atomlist = ['ND1', 'CE3', 'CG1', 'Y', 'CA']
+    >>> len(atomlist)
+    5
+    >>> onehot = atomlist_to_1hot(atomlist)
+    >>> onehot.shape
+    torch.Size([5, 37])
+    >>> onehot[0]
+    tensor([0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    >>> onehot[3]
+    tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+    """
+    mapping = dict(zip(ATOMS, range(len(ATOMS))))
+    classlist = getclasslist(mapping, atomlist)
+    onehot = torch.nn.functional.one_hot(classlist, num_classes=len(ATOMS))
     return onehot
 
 
@@ -65,21 +114,25 @@ def prot_to_graph(pdb, selection=None):
     """
     >>> node_features, edge_index, edge_features = prot_to_graph('1t4e.pdb')
     >>> node_features.shape
-    torch.Size([192, 21])
+    torch.Size([1568, 58])
     >>> edge_index.shape
-    torch.Size([2, 1980])
+    torch.Size([2, 123218])
     >>> edge_features.shape
-    torch.Size([1980])
+    torch.Size([123218])
     """
+    log(f"pdbfile: {pdb}")
     if selection is None:
-        selection = 'polymer.protein and name CB or resn GLY and name CA'
+        selection = 'polymer.protein'
     with pymol2.PyMOL() as p:
         p.cmd.load(pdb, 'prot')
         coords = p.cmd.get_coords(selection=selection)
-        space = {'resnames': [], 'resids': []}
-        p.cmd.iterate(selection=selection, space=space, expression='resnames.append(resn);resids.append(resi)')
+        space = {'resnames': [], 'atomnames': []}
+        p.cmd.iterate(selection=selection, space=space, expression='resnames.append(resn); atomnames.append(name)')
         resnames = np.asarray(space['resnames'])
-    node_features = seq_to_1hot(resnames)
+        atomnames = np.asarray(space['atomnames'])
+    resnames_onehot = seq_to_1hot(resnames)
+    atomnames_onehot = atomlist_to_1hot(atomnames)
+    node_features = torch.cat((resnames_onehot, atomnames_onehot), dim=1)
     dmat = scidist.squareform(scidist.pdist(coords))
     edge_index = torch.tensor(np.asarray(np.where(dmat < 8.)))  # edge_index has shape [2, E] with E the number of edges
     edge_features = torch.tensor(dmat[tuple(edge_index)])
@@ -105,12 +158,6 @@ if __name__ == '__main__':
     import argparse
     # ### UNCOMMENT FOR LOGGING ####
     # import os
-    # import logging
-    # if not os.path.isdir('logs'):
-    #     os.mkdir('logs')
-    # logfilename = 'logs/' + os.path.splitext(os.path.basename(__file__))[0] + '.log'
-    # logging.basicConfig(filename=logfilename, level=logging.INFO, format='%(asctime)s: %(message)s')
-    # logging.info(f"################ Starting {__file__} ################")
     # ### ##################### ####
     # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
     parser = argparse.ArgumentParser(description='')
