7ed229af20bdd05d1029c8a174b35b46cd02e3a2
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-05-12 15:26:43 +0200

    new file:   meld.def 	new file:   meld.sh

---
+++ singularity/meld.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+# shellcheck shell=bash
+# -*- coding: UTF8 -*-
+
+set -e  # exit on error
+set -o noclobber  # prevent overwritting redirection
+
+# Full path to the directory of the current script
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+
+singularity exec -B /c7/scratch2 $DIRSCRIPT/meld.sif meld $@
