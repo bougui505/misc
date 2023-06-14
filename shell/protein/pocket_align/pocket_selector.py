518b75297bd81ae6cb80b9601509bd6165169143
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-06-14 16:18:24 +0200

    new file:   pocket_align/pocket_align.sh 	new file:   pocket_align/pocket_selector.py

---
+++ shell/protein/pocket_align/pocket_selector.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+# -*- coding: UTF8 -*-
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2023 Institut Pasteur                                       #
+#                               				                            #
+#                                                                           #
+#  Redistribution and use in source and binary forms, with or without       #
+#  modification, are permitted provided that the following conditions       #
+#  are met:                                                                 #
+#                                                                           #
+#  1. Redistributions of source code must retain the above copyright        #
+#  notice, this list of conditions and the following disclaimer.            #
+#  2. Redistributions in binary form must reproduce the above copyright     #
+#  notice, this list of conditions and the following disclaimer in the      #
+#  documentation and/or other materials provided with the distribution.     #
+#  3. Neither the name of the copyright holder nor the names of its         #
+#  contributors may be used to endorse or promote products derived from     #
+#  this software without specific prior written permission.                 #
+#                                                                           #
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
+#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
+#                                                                           #
+#  This program is free software: you can redistribute it and/or modify     #
+#                                                                           #
+#############################################################################
+import os
+from os.path import exists
+import pymol2
+
+
+def log(msg):
+    try:
+        logging.info(msg)
+    except NameError:
+        pass
+
+
+def GetScriptDir():
+    scriptpath = os.path.realpath(__file__)
+    scriptdir = os.path.dirname(scriptpath)
+    return scriptdir
+
+
+if __name__ == "__main__":
+    import sys
+    import doctest
+    import argparse
+
+    # ### UNCOMMENT FOR LOGGING ####
+    # import os
+    # import logging
+    # if not os.path.isdir('logs'):
+    #     os.mkdir('logs')
+    # logfilename = 'logs/' + os.path.splitext(os.path.basename(__file__))[0] + '.log'
+    # logging.basicConfig(filename=logfilename, level=logging.INFO, format='%(asctime)s: %(message)s')
+    # logging.info(f"################ Starting {__file__} ################")
+    # ### ##################### ####
+    # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
+    parser = argparse.ArgumentParser(description="")
+    # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
+    parser.add_argument("-p", "--pdb", help="Protein structure file")
+    parser.add_argument(
+        "-l",
+        "--lig",
+        help="Ligand selection or ligand structure file name to define the pocket",
+    )
+    parser.add_argument(
+        "-r", "--radius", help="radius in angstrom to define the pocket", type=float
+    )
+    parser.add_argument("-o", "--out", help="Output filename to save the pocket in")
+    parser.add_argument("--test", help="Test the code", action="store_true")
+    parser.add_argument("--func", help="Test only the given function(s)", nargs="+")
+    args = parser.parse_args()
+
+    # If log is present log the arguments to the log file:
+    for k, v in args._get_kwargs():
+        log(f"# {k}: {v}")
+
+    if args.test:
+        if args.func is None:
+            doctest.testmod(
+                optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE
+            )
+        else:
+            for f in args.func:
+                print(f"Testing {f}")
+                f = getattr(sys.modules[__name__], f)
+                doctest.run_docstring_examples(
+                    f,
+                    globals(),
+                    optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE,
+                )
+        sys.exit()
+
+    if args.pdb is not None:
+        with pymol2.PyMOL() as p:
+            p.cmd.set("fetch_path", os.path.expanduser("~/pdb"))
+            p.cmd.set("fetch_type_default", "mmtf")
+            if os.path.exists(args.pdb):
+                p.cmd.load(filename=args.pdb, object="myprot")
+            else:
+                p.cmd.fetch(args.pdb)
+            if os.path.exists(args.lig):
+                p.cmd.load(filename=args.lig, object="mylig")
+            else:
+                # print(f"Ligand selection: {args.lig}")
+                p.cmd.select(name="mylig", selection=args.lig)
+            natoms = p.cmd.select(
+                name="pocket",
+                selection=f"byres(mylig around {args.radius} and polymer.protein)",
+            )
+            if natoms > 0:
+                p.cmd.save(filename=args.out, selection="pocket")
+            else:
+                print("Empty pocket. Check the selection!")
