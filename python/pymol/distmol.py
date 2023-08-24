b90c79f9348f2fde355c7b4998ce356ad5b3e4c3
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-08-24 13:44:49 +0200

    new file:   distmol.py

---
+++ python/pymol/distmol.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+# -*- coding: UTF8 -*-
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2023 Institut Pasteur                                       #
+#                               				                            #
+#                                                                           #
+#  Redistribution and use in source and binary forms, with or without       #
+#  modification, are permitted provided that the following conditions       #
+#  are met:                                                                 #
+#                                                                           #
+#  1. Redistributions of source code must retain the above copyright        #
+#  notice, this list of conditions and the following disclaimer.            #
+#  2. Redistributions in binary form must reproduce the above copyright     #
+#  notice, this list of conditions and the following disclaimer in the      #
+#  documentation and/or other materials provided with the distribution.     #
+#  3. Neither the name of the copyright holder nor the names of its         #
+#  contributors may be used to endorse or promote products derived from     #
+#  this software without specific prior written permission.                 #
+#                                                                           #
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
+#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
+#                                                                           #
+#  This program is free software: you can redistribute it and/or modify     #
+#                                                                           #
+#############################################################################
+import os
+from misc.protein.coords_loader import get_coords
+from pymol import cmd
+import scipy.spatial.distance as scidist
+
+
+def log(msg):
+    try:
+        logging.info(msg)
+    except NameError:
+        pass
+
+
+def GetScriptDir():
+    scriptpath = os.path.realpath(__file__)
+    scriptdir = os.path.dirname(scriptpath)
+    return scriptdir
+
+
+def get_distance(sel1, sel2, aggr):
+    """"""
+    coords1 = cmd.get_coords(sel1)
+    coords2 = cmd.get_coords(sel2)
+    dmat = scidist.cdist(coords1, coords2)
+    if aggr == "min":
+        return dmat.min()
+    if aggr == "mean":
+        return dmat.mean()
+    else:
+        print(f"Unknown aggregation mode: '{aggr}'")
+        return None
+
+
+if __name__ == "__main__":
+    import sys
+    import doctest
+    import argparse
+
+    # ### UNCOMMENT FOR LOGGING ####
+    # import os
+    # import logging
+    # if not os.path.isdir('logs'):
+    #     os.mkdir('logs')
+    # logfilename = 'logs/' + os.path.splitext(os.path.basename(__file__))[0] + '.log'
+    # logging.basicConfig(filename=logfilename, level=logging.INFO, format='%(asctime)s: %(message)s')
+    # logging.info(f"################ Starting {__file__} ################")
+    # ### ##################### ####
+    # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
+    parser = argparse.ArgumentParser(
+        description="Compute distance between atoms or set of atoms.\
+                     Multiple molecular files can be given (see --pdb).\
+                    "
+    )
+    # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
+    parser.add_argument(
+        "-p",
+        "--pdb",
+        nargs="+",
+        help="Path to molecular structure filename (e.g. pdb) or pdb code. Multiple files/codes can be given. Serial numbering (0-based) is given as object name. The keyword {0} in the selection (see --s1 and --s2) is replaced by the current object number.",
+    )
+    parser.add_argument(
+        "--s1",
+        help="Selection for 1st group of atoms. The keyword {0} can be given to identify the current object.",
+    )
+    parser.add_argument(
+        "--s2",
+        help="Selection for 2nd group of atoms. The keyword {0} can be given to identify the current object.",
+    )
+    parser.add_argument(
+        "-a",
+        "--aggr",
+        help="Aggregation mode to compute distances between group of atoms. Can be: 'min', 'mean'",
+    )
+    parser.add_argument("--test", help="Test the code", action="store_true")
+    parser.add_argument("--func", help="Test only the given function(s)", nargs="+")
+    args = parser.parse_args()
+
+    # If log is present log the arguments to the log file:
+    for k, v in args._get_kwargs():
+        log(f"# {k}: {v}")
+
+    if args.test:
+        if args.func is None:
+            doctest.testmod(
+                optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE
+            )
+        else:
+            for f in args.func:
+                print(f"Testing {f}")
+                f = getattr(sys.modules[__name__], f)
+                doctest.run_docstring_examples(
+                    f,
+                    globals(),
+                    optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE,
+                )
+        sys.exit()
+
+    if args.pdb is not None:
+        for i, pdb in enumerate(args.pdb):
+            coords, selection = get_coords(
+                pdb=pdb, return_selection=True, obj=f"{i}", verbose=False
+            )
+            sel1 = args.s1.format(i)
+            sel2 = args.s2.format(i)
+            distance = get_distance(sel1=sel1, sel2=sel2, aggr=args.aggr)
+            print(f"{i} {pdb} {distance:.4f} Å")
