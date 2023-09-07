6e7db021bfb1ce049a103f8b261121dd1bd70dd0
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-09-07 10:46:43 +0200

    modified:   fun.sh

--- shell/fun.sh
+++ shell/fun.sh
@@ -73,6 +73,9 @@ cat << EOF
 \$(date): sourcing \$0
 EOF
 
+MYTMP=\$(mktemp -d)  # Temporary directory for the current script. Use it to put temporary files.
+trap '/bin/rm -rf "\$MYTMP"' EXIT INT  # Will be removed at the end of the script
+
 precmd() {
     [ -f fun.sh ] && source fun.sh
 }
