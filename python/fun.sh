427e0dbab91efeb96e2052f0e4fb7463b8cd9810
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-09-21 15:02:10 +0200

    new file:   fun.sh 	new file:   tsne.py

---
+++ python/fun.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+cat << EOF
+
+$(date): sourcing $0
+EOF
+
+MYTMP=$(mktemp -d)  # Temporary directory for the current script. Use it to put temporary files.
+trap '/bin/rm -rf "$MYTMP"' EXIT INT  # Will be removed at the end of the script
+
+precmd() {
+    [ -f fun.sh ] && source fun.sh
+}
+
+test_tsne () {
+    paste =(seq 100 | shuf) =(seq 100 | shuf) =(seq 100 | shuf) | ./tsne.py
+}
