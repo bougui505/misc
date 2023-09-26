2a24e0e6680ce09b5213e9b21b0d3a442fdfbe2c
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-09-26 15:39:18 +0200

    modified:   tmscore_format.sh

--- shell/tmscore/tmscore_format.sh
+++ shell/tmscore/tmscore_format.sh
@@ -52,9 +52,9 @@ RMSD=$(echo $TMSCOREOUT | awk '/RMSD of  the common residues=/{print $6}')
 (test -z $SCORE) && SCORE=0.0
 (test -z $RMSD) && RMSD=999.99
 flock $OUT cat << EOF >> $OUT
-model: $MODEL
-native: $NATIVE
-tmscore: $SCORE
-rmsd: $RMSD
-
+model=$MODEL
+native=$NATIVE
+tmscore=$SCORE
+rmsd=$RMSD
+--
 EOF
