e2223b3db35eebdc1701125609ee7f129d7bb2c0
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-10-02 08:53:14 +0200

    modified:   list_fun.sh

--- shell/list_fun.sh
+++ shell/list_fun.sh
@@ -2,5 +2,4 @@
 # shellcheck shell=bash
 # -*- coding: UTF8 -*-
 
-grep -E '^[[:space:]]*([[:alnum:]_]+[[:space:]]*\(\)|function[[:space:]]+[[:alnum:]_]+)' $1
-
+grep -E "^.+[[:space:]]*\(\)[[:space:]]*{" $1 | grep -v "^#"
