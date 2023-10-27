5f49746099adacc9ca3b0e7e2110e3826b88c3ba
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-10-27 12:55:34 +0200

    new file:   MDS.py

---
+++ python/pytorch/MDS.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+# -*- coding: UTF8 -*-
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2023 Institut Pasteur                                       #
+#                               				                            #
+#                                                                           #
+#  Redistribution and use in source and binary forms, with or without       #
+#  modification, are permitted provided that the following conditions       #
+#  are met:                                                                 #
+#                                                                           #
+#  1. Redistributions of source code must retain the above copyright        #
+#  notice, this list of conditions and the following disclaimer.            #
+#  2. Redistributions in binary form must reproduce the above copyright     #
+#  notice, this list of conditions and the following disclaimer in the      #
+#  documentation and/or other materials provided with the distribution.     #
+#  3. Neither the name of the copyright holder nor the names of its         #
+#  contributors may be used to endorse or promote products derived from     #
+#  this software without specific prior written permission.                 #
+#                                                                           #
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
+#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
+#                                                                           #
+#  This program is free software: you can redistribute it and/or modify     #
+#                                                                           #
+#############################################################################
+
+
+import torch
+from tqdm import tqdm
+
+
+class Mover(torch.nn.Module):
+    """
+    >>> mover = Mover(npts=100)
+    >>> x = torch.randn(size=(100, 2))
+    >>> x = mover(x)
+    >>> x.shape
+    torch.Size([100, 2])
+    """
+
+    def __init__(self, npts, ndims=2):
+        super().__init__()
+        self.delta = torch.nn.Parameter(torch.randn(size=(npts, ndims)))
+
+    def forward(self, x):
+        return x + self.delta
+
+
+def lossfunc(x, dmat, repulsion=0.0):
+    """
+    If a dmat value is -1.0 this value is masked in the loss computation
+    >>> y = torch.randn(size=(100, 8))
+    >>> dmat = torch.cdist(y, y)
+    >>> x = torch.randn(size=(100, 2))
+    >>> loss1 = lossfunc(x, dmat)
+    >>> loss1
+    tensor(...)
+
+    Try with a mask
+    >>> dmat[0, 1] = -1.0
+    >>> dmat[10, 5] = -1.0
+    >>> loss2 = lossfunc(x, dmat) 
+    >>> loss2
+    tensor(...)
+    >>> loss2 < loss1
+    tensor(True)
+
+    Try with repulsion
+    >>> loss3 = lossfunc(x, dmat, repulsion=0.01) 
+    >>> loss3
+    tensor(...)
+    >>> loss3 > loss2
+    tensor(True)
+    """
+    xmat = torch.cdist(x, x)
+    mask = (dmat == -1.0)
+    loss_dmat = torch.mean((xmat[~mask] - dmat[~mask])**2)
+    if repulsion > 0.0:
+        repulsive_mask = (xmat < repulsion)
+        loss_rep = torch.mean((xmat[repulsive_mask] - repulsion)**2)
+        return loss_dmat + loss_rep
+    else:
+        return loss_dmat
+
+
+def fit(dmat, repulsion, ndims=2, niter=10000, device='cpu'):
+    npts = dmat.shape[0]
+    x = torch.randn((npts, ndims)).to(device)
+    dmat = dmat.to(device)
+    mover = Mover(npts=npts, ndims=ndims).to(device)
+    optimizer = torch.optim.Adam(mover.parameters(), amsgrad=True, lr=0.01)
+    for i in range(niter):
+        optimizer.zero_grad()
+        y = mover(x)
+        loss = lossfunc(y, dmat, repulsion=repulsion)
+        loss.backward()
+        optimizer.step()
+        progress = (i+1)/niter
+        print(f'{i=}')
+        print(f'{progress=:.2%}')
+        print(f'{loss=:.5g}')
+        print('--')
+
+
+if __name__ == '__main__':
+    import argparse
+    import doctest
+    import sys
+
+    # ### UNCOMMENT FOR LOGGING ####
+    # import os
+    # import logging
+    # if not os.path.isdir('logs'):
+    #     os.mkdir('logs')
+    # logfilename = 'logs/' + os.path.splitext(os.path.basename(__file__))[0] + '.log'
+    # logging.basicConfig(filename=logfilename, level=logging.INFO, format='%(asctime)s: %(message)s')
+    # logging.info(f"################ Starting {__file__} ################")
+    # ### ##################### ####
+    # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
+    parser = argparse.ArgumentParser(description='')
+    # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
+    parser.add_argument(
+        '-r', '--repulsion', help='Repulsion distance (excluded volume)', type=float, default=0.0)
+    parser.add_argument(
+        '-d', '--dim', help='Dimension of the projection space', type=int, default=2)
+    parser.add_argument(
+        '--niter', help='Number of fitting iterations', type=int, default=10000)
+    parser.add_argument('--test', help='Test the code', action='store_true')
+    parser.add_argument(
+        '--test_fit', help='Test fitting random data', action='store_true')
+    parser.add_argument(
+        '--func', help='Test only the given function(s)', nargs='+')
+    args = parser.parse_args()
+
+    DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f"{DEVICE=}")
+
+    if args.test:
+        if args.func is None:
+            doctest.testmod(optionflags=doctest.ELLIPSIS |
+                            doctest.REPORT_ONLY_FIRST_FAILURE)
+        else:
+            for f in args.func:
+                print(f'Testing {f}')
+                f = getattr(sys.modules[__name__], f)
+                doctest.run_docstring_examples(f,
+                                               globals(),
+                                               optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE)
+        sys.exit(0)
+    if args.test_fit:
+        y = torch.randn(size=(100, 8))
+        dmat = torch.cdist(y, y)
+        fit(dmat, repulsion=args.repulsion, ndims=args.dim,
+            niter=args.niter, device=DEVICE)
+        sys.exit(0)
