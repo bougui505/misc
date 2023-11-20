c35e6b7511a481cb0f83fbb3a44bc810a0dc5a4b
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-11-20 14:23:01 +0100

    modified:   project.py 	modified:   pycat.py

--- python/pycat.py
+++ python/pycat.py
@@ -5,39 +5,11 @@
 # Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
 # https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
 # Copyright (c) 2023 Institut Pasteur                                       #
-#                               				                            #
-#                                                                           #
-#  Redistribution and use in source and binary forms, with or without       #
-#  modification, are permitted provided that the following conditions       #
-#  are met:                                                                 #
-#                                                                           #
-#  1. Redistributions of source code must retain the above copyright        #
-#  notice, this list of conditions and the following disclaimer.            #
-#  2. Redistributions in binary form must reproduce the above copyright     #
-#  notice, this list of conditions and the following disclaimer in the      #
-#  documentation and/or other materials provided with the distribution.     #
-#  3. Neither the name of the copyright holder nor the names of its         #
-#  contributors may be used to endorse or promote products derived from     #
-#  this software without specific prior written permission.                 #
-#                                                                           #
-#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
-#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
-#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
-#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
-#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
-#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
-#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
-#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
-#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
-#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
-#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
-#                                                                           #
-#  This program is free software: you can redistribute it and/or modify     #
-#                                                                           #
 #############################################################################
 import os
 
 import numpy as np
+import scipy.spatial.distance as scidist
 import torch
 
 
@@ -54,26 +26,28 @@ def GetScriptDir():
     return scriptdir
 
 
-def py2txt(pyfile, delimiter=" "):
+def py2txt(pyfile, delimiter=" ", squareform=False):
     ext = os.path.splitext(pyfile)[-1]
     if ext == ".pt":
-        data = torch.load(pyfile, map_location=torch.device('cpu'))
+        data = torch.load(pyfile, map_location=torch.device("cpu"))
         data = data.numpy()
     elif ext == ".npy":
         data = np.load(pyfile)
     else:
         sys.exit("Only .pt or .npy extensions are known")
+    if squareform:
+        data = scidist.squareform(data)
     n = data.shape[0]
     for i in range(n):
         line = data[i]
-        if hasattr(line, '__iter__'):
+        if hasattr(line, "__iter__"):
             string = delimiter.join([str(e) for e in line])
         else:
             string = line
         print(string)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import argparse
     import doctest
     import sys
@@ -89,33 +63,46 @@ if __name__ == '__main__':
     # ### ##################### ####
     # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
     parser = argparse.ArgumentParser(
-        description='Read a PyTorch or numpy file (.pt or .npy) and print it as text on the stdout.')
+        description="Read a PyTorch or numpy file (.pt or .npy) and print it as text on the stdout."
+    )
     # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
     parser.add_argument(
-        'inp', help="Input PyTorch file (.pt) or numpy file (.npy) to convert in txt")
+        "inp", help="Input PyTorch file (.pt) or numpy file (.npy) to convert in txt"
+    )
     parser.add_argument(
-        '-d', '--delimiter', help='Column delimiter to use (default 1-space)', default=" ")
-    parser.add_argument('--test', help='Test the code', action='store_true')
+        "-d",
+        "--delimiter",
+        help="Column delimiter to use (default 1-space)",
+        default=" ",
+    )
     parser.add_argument(
-        '--func', help='Test only the given function(s)', nargs='+')
+        "--squareform",
+        help="Apply scipy.spatial.distance.squareform before printing. See: https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.squareform.html",
+        action="store_true",
+    )
+    parser.add_argument("--test", help="Test the code", action="store_true")
+    parser.add_argument("--func", help="Test only the given function(s)", nargs="+")
     args = parser.parse_args()
 
     # If log is present log the arguments to the log file:
     for k, v in args._get_kwargs():
-        log(f'# {k}: {v}')
+        log(f"# {k}: {v}")
 
     if args.test:
         if args.func is None:
-            doctest.testmod(optionflags=doctest.ELLIPSIS |
-                            doctest.REPORT_ONLY_FIRST_FAILURE)
+            doctest.testmod(
+                optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE
+            )
         else:
             for f in args.func:
-                print(f'Testing {f}')
+                print(f"Testing {f}")
                 f = getattr(sys.modules[__name__], f)
-                doctest.run_docstring_examples(f,
-                                               globals(),
-                                               optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE)
+                doctest.run_docstring_examples(
+                    f,
+                    globals(),
+                    optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE,
+                )
         sys.exit()
 
     if args.inp is not None:
-        py2txt(args.inp, args.delimiter)
+        py2txt(args.inp, args.delimiter, squareform=args.squareform)
