fabdcc007de0a12704342506a7ff3aff5d4f21fd
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-11-21 14:34:56 +0100

    modified:   recawk.sh 	modified:   recawk_test/fun.sh

--- shell/recawk.sh
+++ shell/recawk.sh
@@ -1,40 +1,10 @@
 #!/usr/bin/env bash
-# shellcheck shell=bash
 # -*- coding: UTF8 -*-
 
 #############################################################################
 # Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
 # https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
 # Copyright (c) 2023 Institut Pasteur                                       #
-#                 				                            #
-#                                                                           #
-#  Redistribution and use in source and binary forms, with or without       #
-#  modification, are permitted provided that the following conditions       #
-#  are met:                                                                 #    
-#                                                                           #
-#  1. Redistributions of source code must retain the above copyright        #
-#  notice, this list of conditions and the following disclaimer.            #
-#  2. Redistributions in binary form must reproduce the above copyright     #
-#  notice, this list of conditions and the following disclaimer in the      #
-#  documentation and/or other materials provided with the distribution.     #
-#  3. Neither the name of the copyright holder nor the names of its         #
-#  contributors may be used to endorse or promote products derived from     #
-#  this software without specific prior written permission.                 #
-#                                                                           #
-#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
-#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
-#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
-#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
-#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
-#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
-#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
-#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
-#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
-#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
-#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
-#                                                                           #
-#  This program is free software: you can redistribute it and/or modify     #
-#                                                                           #
 #############################################################################
 
 set -e  # exit on error
@@ -99,6 +69,10 @@ An ${bold}END${normal} can be given as in standard awk to run a command when awk
 
     ${bold}zcat data/file.rec.gz | recawk '{a[nr]=rec["i"]}END{for (i in a){print i, a[i]}}'${normal}
 
+${bold}-v${normal} can be given as in standard awk command. E.g. ${bold}recawk -v "A=1" '{...}'${normal}
+
+    ${bold}zcat data/file.rec.gz | recawk -v "ania=ciao" '{printrec();print("ania="ania);print("--")}'${normal}
+
 Examples:
     
 ${bold} 
@@ -107,13 +81,16 @@ ${bold}
     zcat data/file.rec.gz | recawk '{printrec();print("k=v");print("--")}
     zcat data/file.rec.gz | recawk '{printrec();print("nr="nr);print("NR="NR);print("--")}'
     zcat data/file.rec.gz | recawk '{a[nr]=rec["i"]}END{for (i in a){print i, a[i]}}'
+    zcat data/file.rec.gz | recawk -v "ania=ciao" '{printrec();print("ania="ania);print("--")}'
 ${normal} 
 
 EOF
 }
 
+V="V=0"
 case $1 in
     -h|--help) usage; exit 0 ;;
+    -v) shift; V=$1; shift ;;
 esac
 
 if [ "$#" -eq 0 ]; then
@@ -124,7 +101,8 @@ CMD=$(echo $1 | awk -F"END" '{print $1}')
 ENDCMD=$(echo $1 | awk -F"END" '{print $2}')
 FILENAMES="${@:2}"
 
-awk -F"=" '
+echo $V
+awk -v $V -F"=" '
 function printrec(){
     for (field in rec){
         print field"="rec[field]
