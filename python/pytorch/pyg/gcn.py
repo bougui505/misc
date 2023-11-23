b98d34efd1d802afddcd948c575605d8f2edd8d1
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-11-23 12:55:17 +0100

    modified:   ../pytorch/pyg/gcn.py

--- python/pytorch/pyg/gcn.py
+++ python/pytorch/pyg/gcn.py
@@ -5,40 +5,13 @@
 # Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
 # https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
 # Copyright (c) 2022 Institut Pasteur                                       #
-#                 				                            #
-#                                                                           #
-#  Redistribution and use in source and binary forms, with or without       #
-#  modification, are permitted provided that the following conditions       #
-#  are met:                                                                 #
-#                                                                           #
-#  1. Redistributions of source code must retain the above copyright        #
-#  notice, this list of conditions and the following disclaimer.            #
-#  2. Redistributions in binary form must reproduce the above copyright     #
-#  notice, this list of conditions and the following disclaimer in the      #
-#  documentation and/or other materials provided with the distribution.     #
-#  3. Neither the name of the copyright holder nor the names of its         #
-#  contributors may be used to endorse or promote products derived from     #
-#  this software without specific prior written permission.                 #
-#                                                                           #
-#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
-#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
-#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
-#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
-#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
-#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
-#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
-#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
-#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
-#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
-#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
-#                                                                           #
-#  This program is free software: you can redistribute it and/or modify     #
-#                                                                           #
 #############################################################################
+
 import os
+
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv, GATConv
+from torch_geometric.nn import GATConv, GCNConv
 
 
 class GCN(torch.nn.Module):
@@ -56,6 +29,7 @@ class GCN(torch.nn.Module):
     torch.Size([3, 4])
 
     """
+
     def __init__(self, num_node_features, num_classes):
         super().__init__()
         self.conv1 = GCNConv(num_node_features, 16)
@@ -89,6 +63,7 @@ class GAT(torch.nn.Module):
     >>> outweight.shape
     torch.Size([7, 1])
     """
+
     def __init__(self, num_node_features, num_classes):
         super().__init__()
         self.conv1 = GATConv(num_node_features, 16, add_self_loops=False)
@@ -98,7 +73,11 @@ class GAT(torch.nn.Module):
         x = self.conv1(x, edge_index, edge_weight)
         x = F.relu(x)
         x = F.dropout(x, training=self.training)
-        x, (out_edge_index, out_edge_weight) = self.conv2(x, edge_index, edge_weight, return_attention_weights=True)
+        x, (out_edge_index,
+            out_edge_weight) = self.conv2(x,
+                                          edge_index,
+                                          edge_weight,
+                                          return_attention_weights=True)
         return F.log_softmax(x, dim=1), out_edge_index, out_edge_weight
 
 
@@ -116,9 +95,10 @@ def GetScriptDir():
 
 
 if __name__ == '__main__':
-    import sys
-    import doctest
     import argparse
+    import doctest
+    import sys
+
     # ### UNCOMMENT FOR LOGGING ####
     # import os
     # import logging
@@ -133,7 +113,9 @@ if __name__ == '__main__':
     # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
     parser.add_argument('-a', '--arg1')
     parser.add_argument('--test', help='Test the code', action='store_true')
-    parser.add_argument('--func', help='Test only the given function(s)', nargs='+')
+    parser.add_argument('--func',
+                        help='Test only the given function(s)',
+                        nargs='+')
     args = parser.parse_args()
 
     # If log is present log the arguments to the log file:
@@ -142,7 +124,8 @@ if __name__ == '__main__':
 
     if args.test:
         if args.func is None:
-            doctest.testmod(optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE)
+            doctest.testmod(optionflags=doctest.ELLIPSIS
+                            | doctest.REPORT_ONLY_FIRST_FAILURE)
         else:
             for f in args.func:
                 print(f'Testing {f}')
