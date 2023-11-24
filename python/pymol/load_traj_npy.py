cbeb76b6de8906614de245efdf48b2c0af650e73
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-11-24 09:22:54 +0100

    modified:   ../protein/graph/proteingraph.py 	modified:   load_traj_npy.py

--- python/pymol/load_traj_npy.py
+++ python/pymol/load_traj_npy.py
@@ -5,44 +5,20 @@
 # Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
 # https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
 # Copyright (c) 2022 Institut Pasteur                                       #
-#                 				                            #
-#                                                                           #
-#  Redistribution and use in source and binary forms, with or without       #
-#  modification, are permitted provided that the following conditions       #
-#  are met:                                                                 #
-#                                                                           #
-#  1. Redistributions of source code must retain the above copyright        #
-#  notice, this list of conditions and the following disclaimer.            #
-#  2. Redistributions in binary form must reproduce the above copyright     #
-#  notice, this list of conditions and the following disclaimer in the      #
-#  documentation and/or other materials provided with the distribution.     #
-#  3. Neither the name of the copyright holder nor the names of its         #
-#  contributors may be used to endorse or promote products derived from     #
-#  this software without specific prior written permission.                 #
-#                                                                           #
-#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
-#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
-#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
-#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
-#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
-#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
-#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
-#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
-#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
-#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
-#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
-#                                                                           #
-#  This program is free software: you can redistribute it and/or modify     #
-#                                                                           #
 #############################################################################
 
-from pymol import cmd
 import numpy as np
+from pymol import cmd
 
 
 def create_topology(coords, object):
     for i, c in enumerate(coords):
-        cmd.pseudoatom(object, pos=list(c), resi=i + 1, name='CA', resn='GLY', hetatm=False)
+        cmd.pseudoatom(object,
+                       pos=list(c),
+                       resi=i + 1,
+                       name='CA',
+                       resn='GLY',
+                       hetatm=False)
         if i > 0:
             cmd.bond(f'resi {i + 1}', f'resi {i}')
     cmd.color('green', object)
@@ -68,15 +44,19 @@ def load_traj_npy(npyfile, topology=None, selection='all', object='traj'):
     state = 2
     for i in range(startindex, nsteps):
         coords = traj[i]
-        cmd.create(object, selection=object, source_state=state - 1, target_state=state)
+        cmd.create(object,
+                   selection=object,
+                   source_state=state - 1,
+                   target_state=state)
         cmd.load_coords(coords, object, state=state)
         state += 1
 
 
 if __name__ == '__main__':
-    import sys
-    import doctest
     import argparse
+    import doctest
+    import sys
+
     # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
     parser = argparse.ArgumentParser(description='')
     # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
@@ -85,5 +65,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.test:
-        doctest.testmod(optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE)
+        doctest.testmod(optionflags=doctest.ELLIPSIS
+                        | doctest.REPORT_ONLY_FIRST_FAILURE)
         sys.exit()
