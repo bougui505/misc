795a65bc6a8f9fdadd75352e95407aac0a25e03f
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-12-06 12:25:48 +0100

    new file:   awkpaste.sh 	new file:   awkpaste_test/fun.sh

---
+++ shell/awkpaste_test/fun.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+Cyan='\033[0;36m'
+NC='\033[0m' # No Color
+
+echo -e "$Cyan"
+cat << EOF
+$(date): sourcing $0
+EOF
+echo -e $FUNFILES$NC
+
+MYTMP=$(mktemp -d)  # Temporary directory for the current script. Use it to put temporary files.
+trap '/bin/rm -rf "$MYTMP"' EXIT INT  # Will be removed at the end of the script
+
+[ -z $FUNFILES ] && export FUNFILES=""
+precmd() {
+    [ -f fun.sh ] && source fun.sh && FUNFILES+="$(realpath fun.sh)" && FUNFILES=$(echo $FUNFILES | awk -F":" '{for (i=1;i<=NF;i++){print $i}}' | sort -u | awk '{printf $1":"}')
+}
+
+test1 () {
+    ../awkpaste.sh =(seq 10) =(seq 13)
+}
+
+test2 () {
+    ../awkpaste.sh =(seq 13) =(seq 10)
+}
