9acc656cc975dd07bde10848a22fa4a757436f38
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-12-11 15:10:27 +0100

    new file:   squash.sh 	new file:   unsquash.sh

---
+++ shell/squash/squash.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+# -*- coding: UTF8 -*-
+
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr
+# https://research.pasteur.fr/en/member/guillaume-bouvier/
+# 2017-12-04 11:00:53 (UTC+0100)
+
+# Make a squashfs archive of the file
+
+ARCHIVE_DIR="."
+
+INPATH=$1
+
+BASENAME=$(basename "$INPATH")
+OUTPATH="$ARCHIVE_DIR/$BASENAME.sqsh"
+
+if [ ! -f $OUTPATH ]; then
+    mksquashfs $INPATH $OUTPATH -comp xz && echo "archive $OUTPATH successfully created"
+else
+    (>&2 echo "$OUTPATH already exists. Cannot create archive...")
+fi
