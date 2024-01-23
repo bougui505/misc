73f59310df113c49ccc4d2e78df9c0c94c41f63d
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2024-01-23 09:45:19 +0100

    new file:   tsp_running_time.sh

---
+++ shell/tsp_running_time.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2023 Institut Pasteur                                       #
+#############################################################################
+#
+# creation_date: Mon Jan 22 13:36:54 2024
+
+set -e  # exit on error
+set -o pipefail  # exit when a process in the pipe fails
+set -o noclobber  # prevent overwritting redirection
+
+# Full path to the directory of the current script
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+
+qalc -t $(tsp -i | awk -F":" '{if ($1=="Time running"){print $2}}')
