1bb99c024208ee79cbd1ea4e165f893bcd38ebdd
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2024-02-08 13:24:46 +0100

    Subject: Implement SmiDataset for file processing and add --file option
    This commit introduces the following changes:
    
    Added SmiDataset class: This class is used to load and process files containing pairs of SMILES strings and additional information.
    Added process_file function: This function takes a file path as input and reads the file line by line. For each line, it extracts the SMILES strings and information, calculates the fingerprint similarity, and writes the results to a new file with an added similarity column.
    Expanded argument parser: The argparse has been updated to include a new --file argument that allows users to specify a file path for processing.
    Minor code formatting: Some minor adjustments to code formatting have been made for consistency.
    This feature allows users to efficiently calculate fingerprint similarity for multiple SMILES string pairs stored in a file.
    
    	modified:   molsim.py

--- python/mols/molsim.py
+++ python/mols/molsim.py
@@ -9,8 +9,12 @@
 # creation_date: Wed Feb  7 15:26:27 2024
 
 
+import os
+
 from rdkit import Chem, DataStructs
 from rdkit.Chem import rdRascalMCES
+from torch.utils.data import DataLoader, Dataset
+from tqdm import tqdm
 
 
 def rascal(smi1, smi2):
@@ -49,7 +53,42 @@ def fpsim(smi1, smi2):
     sim = DataStructs.FingerprintSimilarity(fs1, fs2)
     return sim
 
-import os
+class SmiDataset(Dataset):
+    def __init__(self, filename) -> None:
+        super().__init__()
+        with open(filename, "r") as f:
+            self.lines = f.read().splitlines()
+    def __len__(self):
+        return len(self.lines)
+    def __getitem__(self, index):
+        line = self.lines[index]
+        data = line.split()
+        smi1, smi2 = data[:2]
+        info = data[2:]
+        sim = fpsim(smi1, smi2)
+        out = [smi1, smi2]
+        out.extend(info)
+        out.append(sim)
+        return out
+
+def process_file(filename):
+    """
+    """
+    outfilename = os.path.splitext(filename)[0] + "_sim" + ".txt"
+    smidataset = SmiDataset(filename)
+    smidataloader = DataLoader(smidataset, batch_size=os.cpu_count(), shuffle=False, num_workers=os.cpu_count())
+    with open(outfilename, "w") as outfile:
+        for batch in tqdm(smidataloader):
+            # print(batch)
+            smi1_batch = batch[0]
+            smi2_batch =  batch[1]
+            info_batch = batch[2:-1]
+            sim_batch = batch[-1].numpy()
+            out = list(zip(smi1_batch, smi2_batch, *info_batch, sim_batch))
+            for e in out:
+                e = [str(_) for _ in e]
+                outfile.write(" ".join(e) + "\n")
+
 
 if __name__ == "__main__":
     import argparse
@@ -59,6 +98,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Compute molecular similarity between the 2 given smiles smi1 and smi2")
     parser.add_argument("--smi1", help='First SMILES string', metavar="'Oc1cccc2C(=O)C=CC(=O)c12'")
     parser.add_argument("--smi2", help='Second SMILES string', metavar="'O1C(=O)C=Cc2cc(OC)c(O)cc12'")
+    parser.add_argument("--file", help='Process the given file with the following line format: smi1 smi2 [info1] [...] [infon]. The result will be printed in the last column')
     parser.add_argument("--test", help="Test the code", action="store_true")
     parser.add_argument("--func", help="Test only the given function(s)", nargs="+")
     args = parser.parse_args()
@@ -81,3 +121,5 @@ if __name__ == "__main__":
     if args.smi1 is not None and args.smi2 is not None:
         sim = fpsim(args.smi1, args.smi2)
         print(f"{sim:.2g}")
+    if args.file is not None:
+        process_file(args.file)
