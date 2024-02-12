336ba9b90dff041f29fdb3c00e33c98264b19297
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2024-02-12 14:04:32 +0100

    **Subject:** Add script "molsim.sh" for convenient execution of `molsim.py`
    This commit introduces a new shell script named `molsim.sh`. This script:
    
    * Simplifies running the `molsim.py` script by automatically setting the script location using `DIRSCRIPT`.
    * Accepts arguments directly without requiring the full path to `molsim.py`.
    * Provides a more user-friendly and consistent way to interact with `molsim.py`.
    
    	new file:   molsim.sh

---
+++ python/mols/molsim.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2024 Institut Pasteur                                       #
+#############################################################################
+#
+# creation_date: Mon Feb 12 14:02:18 2024
+
+set -e  # exit on error
+set -o pipefail  # exit when a process in the pipe fails
+set -o noclobber  # prevent overwritting redirection
+
+# Full path to the directory of the current script
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+
+sing $DIRSCRIPT/molsim.py $@
