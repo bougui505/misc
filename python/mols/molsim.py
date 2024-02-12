8dea23eaa63e804b47ee6c2d5a8fb8c17d6dcb7c
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2024-02-12 13:56:05 +0100

    **Subject: Enhance molsim.py with validity checks, RecDataset, and rec file processing**
    This commit introduces several improvements to `molsim.py`:
    
    * **Validity checks:** Added `isvalid` function to verify if a given SMILES string represents a valid molecule using RDKit. This ensures accuracy in similarity calculations.
    * **RecDataset:** Introduced a new `RecDataset` class to easily process similarity metrics from data stored in `.rec.gz` files. It enables specifying specific keys for accessing SMILES strings within the record.
    * **Rec file processing:** Added `process_recfile` function to calculate and store similarity scores between SMILES strings extracted from a `.rec.gz` file based on given keys. This automates and streamlines similarity analysis for datasets stored in this format.
    * **Improved file processing:** Renamed `process_file` to `process_smifile` for clarity and added checks for `args.rec` flag before calling the appropriate processing function.
    * **Command-line arguments:** Updated argument descriptions in `__main__` to reflect the newly added `--rec` option and the possibility of using keys for `smi1` and `smi2` when using `.rec` files.
    
    These changes enhance the functionality and versatility of `molsim.py` for efficient and reliable similarity analysis of both individual SMILES strings and sets of molecules stored in `.rec.gz` formats.
    
    	modified:   molsim.py

--- python/mols/molsim.py
+++ python/mols/molsim.py
@@ -16,6 +16,8 @@ from rdkit.Chem import rdRascalMCES
 from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
 
+from misc import rec
+
 
 def rascal(smi1, smi2):
     """
@@ -71,7 +73,7 @@ class SmiDataset(Dataset):
         out.append(sim)
         return out
 
-def process_file(filename):
+def process_smifile(filename):
     """
     """
     outfilename = os.path.splitext(filename)[0] + "_sim" + ".txt"
@@ -89,6 +91,50 @@ def process_file(filename):
                 e = [str(_) for _ in e]
                 outfile.write(" ".join(e) + "\n")
 
+def isvalid(smi):
+    if smi is not None and len(smi) > 0:
+        mol = Chem.MolFromSmiles(smi)
+        if mol is not None:
+            valid = True
+        else:
+            valid = False
+    else:
+        valid = False
+    return valid
+
+class RecDataset(Dataset):
+    """
+    >>> recdataset = RecDataset(recfile='molsim_test.rec.gz', key1='smi_gen', key2='smi_ref')
+    >>> recdataset[0]
+    0.33902759526938236
+    """
+    def __init__(self, recfile, key1, key2) -> None:
+        super().__init__()
+        self.data, self.fields = rec.get_data(file=recfile)
+        self.key1 = key1
+        self.key2 = key2
+    def __len__(self):
+        return len(self.data[self.key1])
+    def __getitem__(self, index):
+        smi1, smi2 = self.data[self.key1][index], self.data[self.key2][index]
+        smi1 = smi1.replace("'", "")
+        smi2 = smi2.replace("'", "")
+        if isvalid(smi1) and isvalid(smi2):
+            sim = fpsim(smi1, smi2)
+        else:
+            sim = -1.
+        return sim
+
+def process_recfile(recfile, key1, key2):
+    recdataset = RecDataset(recfile=recfile, key1=key1, key2=key2)
+    outfilename = os.path.splitext(recfile)[0] + "_sim" + ".rec.gz"
+    recdataloader = DataLoader(recdataset, batch_size=os.cpu_count(), shuffle=False, num_workers=os.cpu_count())
+    similarities = []
+    for batch in tqdm(recdataloader):
+        similarities.extend(list(batch.numpy()))
+    recdataset.data["sim"] = similarities
+    rec.dict_to_rec(recdataset.data, outgz=outfilename)
+
 
 if __name__ == "__main__":
     import argparse
@@ -96,9 +142,10 @@ if __name__ == "__main__":
     import sys
 
     parser = argparse.ArgumentParser(description="Compute molecular similarity between the 2 given smiles smi1 and smi2")
-    parser.add_argument("--smi1", help='First SMILES string', metavar="'Oc1cccc2C(=O)C=CC(=O)c12'")
-    parser.add_argument("--smi2", help='Second SMILES string', metavar="'O1C(=O)C=Cc2cc(OC)c(O)cc12'")
+    parser.add_argument("--smi1", help='First SMILES string, or rec key if --rec is given', metavar="['Oc1cccc2C(=O)C=CC(=O)c12', 'smi_gen']")
+    parser.add_argument("--smi2", help='Second SMILES string, or rech key if --rec is given', metavar="['O1C(=O)C=Cc2cc(OC)c(O)cc12', 'smi_ref']")
     parser.add_argument("--file", help='Process the given file with the following line format: smi1 smi2 [info1] [...] [infon]. The result will be printed in the last column')
+    parser.add_argument("--rec", help='Process the given rec file. The key to read smi1 and smi2 are read from options --smi1 and --smi2 respectively.', metavar='molsim_test.rec.gz')
     parser.add_argument("--test", help="Test the code", action="store_true")
     parser.add_argument("--func", help="Test only the given function(s)", nargs="+")
     args = parser.parse_args()
@@ -118,8 +165,10 @@ if __name__ == "__main__":
                     optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE,
                 )
         sys.exit()
-    if args.smi1 is not None and args.smi2 is not None:
+    if args.smi1 is not None and args.smi2 is not None and args.rec is None:
         sim = fpsim(args.smi1, args.smi2)
         print(f"{sim:.2g}")
     if args.file is not None:
-        process_file(args.file)
+        process_smifile(args.file)
+    if args.rec is not None:
+        process_recfile(recfile=args.rec, key1=args.smi1, key2=args.smi2)
