f08054ba3bf4cae9d89b2e7bd4b7f076ec978e22
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2024-02-27 23:51:32 +0100

    new file:   remind/agenda.sh

---
+++ shell/remind/agenda.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2024 Institut Pasteur                                       #
+#############################################################################
+#
+# creation_date: Tue Feb 27 23:38:21 2024
+
+set -e  # exit on error
+set -o pipefail  # exit when a process in the pipe fails
+set -o noclobber  # prevent overwritting redirection
+
+# Full path to the directory of the current script
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+
+function usage () {
+    cat << EOF
+Print an agenda using remind: rem -b1 -n
+with an optionnal start date (YYYY/MM/DD):
+rem -b1 -n YYYY/MM/DD
+
+Usage:
+    -h, --help print this help message and exit
+    -d, --date date to start from (YYYY/MM/DD)
+EOF
+}
+
+DATE=""
+while [ "$#" -gt 0 ]; do
+    case $1 in
+        -h|--help) usage; exit 0 ;;
+        -d|--date) DATE=$2; shift ;;
+        --) OTHER="${@:2}";break; shift;;  # Everything after the '--' symbol
+    esac
+    shift
+done
+
+
+function agenda () {
+    if [[ -z $1 ]]; then
+        DATE=$(date +%F | tr - /)
+    else
+        DATE=$1  # Optionnal start date (YYYY/MM/DD)
+    fi
+    rem -b1 -n $DATE | sort -r | grep --color "$DATE\|$"
+}
+
+echo $DATE
+agenda $DATE
