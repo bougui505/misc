78e69edc7cea6177beb231d897ae9fa6a25e531b
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2024-04-08 10:35:05 +0200

    new file:   ../keepass_backup.sh

---
+++ shell/keepass_backup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2024 Institut Pasteur                                       #
+#############################################################################
+#
+# creation_date: Fri Apr  5 09:31:21 2024
+
+set -e  # exit on error
+set -o pipefail  # exit when a process in the pipe fails
+set -o noclobber  # prevent overwritting redirection
+
+# Full path to the directory of the current script
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+
+if [[ -d /media/bougui/stick16G ]]; then
+    rsync -a --update --progress -h $HOME/Passwords.kdbx /media/bougui/stick16G/
+fi
