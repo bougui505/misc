1cd51a0c8df4ce543b0365c9fae89c60b1a90708
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2024-04-29 15:40:03 +0200

    new file:   zcat.sh

---
+++ shell/zcat.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2024 Institut Pasteur                                       #
+#############################################################################
+#
+# creation_date: Mon Apr 29 15:28:59 2024
+# Custom zcat with progress bar
+
+set -e  # exit on error
+set -o pipefail  # exit when a process in the pipe fails
+set -o noclobber  # prevent overwritting redirection
+
+# Full path to the directory of the current script
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+# MYTMP=$(mktemp -d)  # Temporary directory for the current script. Use it to put temporary files.
+# trap 'rm -rvf "$MYTMP"' EXIT INT  # Will be removed at the end of the script
+
+FILENAMES=$@
+TOTALBYTES=$(du -sb $FILENAMES|awk '{S+=$1}END{print S}')
+cat $FILENAMES | pv -p -e -b -s $TOTALBYTES | zcat 2> /dev/null
+
