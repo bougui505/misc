7a2c80ae61905fd062e24523734450a88047cb10
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2024-06-05 10:37:35 +0200

    new file:   dockprep.sh 	modified:   pdbselect.py         --dockprep option added: Run chimera dockprep command to sanitize, compute charges, ..., before writing a mol2 file

--- python/protein/pdbselect.py
+++ python/protein/pdbselect.py
@@ -5,37 +5,10 @@
 # Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
 # https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
 # Copyright (c) 2022 Institut Pasteur                                       #
-#                 				                            #
-#                                                                           #
-#  Redistribution and use in source and binary forms, with or without       #
-#  modification, are permitted provided that the following conditions       #
-#  are met:                                                                 #
-#                                                                           #
-#  1. Redistributions of source code must retain the above copyright        #
-#  notice, this list of conditions and the following disclaimer.            #
-#  2. Redistributions in binary form must reproduce the above copyright     #
-#  notice, this list of conditions and the following disclaimer in the      #
-#  documentation and/or other materials provided with the distribution.     #
-#  3. Neither the name of the copyright holder nor the names of its         #
-#  contributors may be used to endorse or promote products derived from     #
-#  this software without specific prior written permission.                 #
-#                                                                           #
-#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
-#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
-#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
-#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
-#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
-#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
-#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
-#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
-#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
-#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
-#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
-#                                                                           #
-#  This program is free software: you can redistribute it and/or modify     #
-#                                                                           #
 #############################################################################
+
 import os
+import subprocess
 
 from pymol import cmd
 
@@ -75,6 +48,7 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--out')
     parser.add_argument('--h_add', help="Add hydrogens", action='store_true')
     parser.add_argument('--h_remove', help="Remove hydrogens", action='store_true')
+    parser.add_argument('--dockprep', help="Run chimera dockprep command to sanitize, compute charges, ..., before writing a mol2 file", action='store_true')
     parser.add_argument('--test', help='Test the code', action='store_true')
     parser.add_argument('--func', help='Test only the given function(s)', nargs='+')
     args = parser.parse_args()
@@ -99,3 +73,5 @@ if __name__ == '__main__':
     if args.h_add:
         cmd.h_add("all")
     cmd.save(args.out, selection=args.select)
+    if args.dockprep:
+        subprocess.Popen([f"{GetScriptDir()}/dockprep.sh", "-i", f"{args.out}", "-o", f"{args.out}"])
