1e96a5f13f753f0935aae600bc8931875077fb8c
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2024-06-06 09:32:37 +0200

    new file:   tsp_status.sh

---
+++ shell/tsp_status.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2024 Institut Pasteur                                       #
+#############################################################################
+#
+# creation_date: Thu Jun  6 09:07:06 2024
+
+set -e  # exit on error
+set -o pipefail  # exit when a process in the pipe fails
+set -o noclobber  # prevent overwritting redirection
+
+# Full path to the directory of the current script
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+MYTMP=$(mktemp -d)  # Temporary directory for the current script. Use it to put temporary files.
+trap 'rm -rf "$MYTMP"' EXIT INT  # Will be removed at the end of the script
+
+function usage () {
+    cat << EOF
+Help message
+    -h, --help print this help message and exit
+EOF
+}
+
+N=1  # Default value
+while [ "$#" -gt 0 ]; do
+    case $1 in
+        -n|--number) N="$2"; shift ;;
+        -h|--help) usage; exit 0 ;;
+        --) OTHER="${@:2}";break; shift;;  # Everything after the '--' symbol
+        *) usage; exit 1 ;;
+    esac
+    shift
+done
+
+tsp | awk 'NR>1{
+            status=$2
+            n[status]+=1
+            if (status=="finished"){
+                split($5,a,"/")
+                t+=a[1]
+            }
+           }
+           END{
+            for (status in n){
+                print status,n[status]
+            }
+            t_mean=t/n["finished"]"s"
+            "qalc -t "t_mean | getline t_mean_fmt
+            print "average_running_time "t_mean_fmt
+            progress=n["finished"]/(n["queued"]+n["running"]+n["finished"])
+            print("progress "progress*100" %")
+           }' > $MYTMP/tsp_status.out
+cat $MYTMP/tsp_status.out
