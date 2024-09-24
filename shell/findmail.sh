6554d0c04a01e34a2d52dd83aa9d5cf4323766ac
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2024-09-24 15:37:20 +0200

    new file:   findmail.sh

---
+++ shell/findmail.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env zsh
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2024 Institut Pasteur                                       #
+#############################################################################
+#
+# creation_date: Tue Sep 24 14:40:47 2024
+
+set -e  # exit on error
+set -o pipefail  # exit when a process in the pipe fails
+set -o noclobber  # prevent overwritting redirection
+
+# Full path to the directory of the current script
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+MYTMP=$(mktemp -d)  # Temporary directory for the current script. Use it to put temporary files.
+trap 'rm -rf "$MYTMP"' EXIT INT  # Will be removed at the end of the script
+
+MAILDIR="/home/bougui/mails/mutt"
+touch $MAILDIR/mail.list
+
+rga-fzf() {
+    cd $MAILDIR/gzips
+    RG_PREFIX="rga --files-with-matches --rga-adapters=+mail"
+    local file
+    query_file="$(
+        FZF_DEFAULT_COMMAND="$RG_PREFIX '$1'" \
+            fzf --multi --color=light --sort --preview="[[ ! -z {} ]] && rga --rga-adapters=+mail --pretty --context 1 {q} {}" \
+            --phony -q "$1" \
+            --bind "change:reload:$RG_PREFIX {q}" \
+            --preview-window="70%:wrap")"
+    cd -
+    for x in $(echo $query_file); do
+        echo $MAILDIR/gzips/$x
+    done
+}
+
+find $MAILDIR/backup -type f > $MYTMP/mail.list
+
+gawk '{
+if (ARGIND==1){
+    DONE[$0]=$0
+}
+if (ARGIND==2){
+    if (!($0 in DONE)){
+        print $0
+    }
+}
+}' $MAILDIR/mail.list $MYTMP/mail.list > $MYTMP/todo.list
+
+for mail in $(cat $MYTMP/todo.list); do
+    GZIPDIR=$MAILDIR/gzips/$(echo "${mail:h}"|sed "s,$MAILDIR/,,")
+    mkdir -p $GZIPDIR
+    GZIPFILE=$GZIPDIR"/$mail:t.gz"
+    echo "Creating: $GZIPFILE"
+    mu view $mail | gzip > "$GZIPFILE" \
+        && echo $mail >> $MAILDIR/mail.list
+done
+
+rga-fzf
