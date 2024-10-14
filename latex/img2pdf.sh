d20da39fc1bd4d7526380508826d0fa7e39d81a7
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2024-10-14 15:09:45 +0200

    modified:   img2pdf.sh

--- latex/img2pdf.sh
+++ latex/img2pdf.sh
@@ -12,6 +12,10 @@ set -e  # exit on error
 set -o pipefail  # exit when a process in the pipe fails
 set -o noclobber  # prevent overwritting redirection
 
+function usage(){
+  echo img2pdf inputfile
+}
+
 # Full path to the directory of the current script
 DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
 MYTMP=$(mktemp -d)  # Temporary directory for the current script. Use it to put temporary files.
@@ -19,6 +23,8 @@ trap 'rm -rf "$MYTMP"' EXIT INT  # Will be removed at the end of the script
 
 IMGFILE=$1
 OUTFILE=$IMGFILE:r.pdf
+[[ -z $IMGFILE ]] && usage && exit 1
+[[ ! -f $IMGFILE ]] && usage && exit 1
 echo "$IMGFILE -> $OUTFILE"
 inkscape $1 -o $OUTFILE
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.5 -dEmbedAllFonts=true -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$MYTMP/$OUTFILE:t $OUTFILE
