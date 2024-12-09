5ef03c02ef0044eef6743a883264643828f1db05
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2024-12-09 11:38:05 +0100

    new file:   json2rec.sh

---
+++ shell/json2rec.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2024 Institut Pasteur                                       #
+#############################################################################
+#
+# creation_date: Mon Dec  9 11:29:25 2024
+
+set -e  # exit on error
+set -o pipefail  # exit when a process in the pipe fails
+set -o noclobber  # prevent overwritting redirection
+
+# Full path to the directory of the current script
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+# MYTMP=$(mktemp -d)  # Temporary directory for the current script. Use it to put temporary files.
+# trap 'rm -rvf "$MYTMP"' EXIT INT  # Will be removed at the end of the script
+
+JSONFILE=$1
+
+jq -r 'to_entries[] | "\(.key): \(.value)"' \
+    $JSONFILE
+echo "--"
