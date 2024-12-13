881aab72bdc6af7e88e53bb8acf3f49575fe6bb8
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2024-12-13 09:47:18 +0100

    modified:   mols/ligrmsd.py

--- python/mols/ligrmsd.py
+++ python/mols/ligrmsd.py
@@ -11,10 +11,6 @@
 import numpy as np
 from rdkit import Chem
 
-molref = Chem.MolFromMol2File('NCT.mol2', sanitize=True)
-suppl = Chem.SDMolSupplier('6CNK_ECD_A4A4_noNCT_Ligands.sdf', sanitize=True)
-# suppl = Chem.SDMolSupplier('Nico3bp.sdf', sanitize=True)
-
 
 def get_coords(mol):
     coords = []
@@ -25,15 +21,38 @@ def get_coords(mol):
     coords = np.asarray(coords)
     return coords
 
-coord_ref = get_coords(molref)
-
-
-for mol in suppl:
+def get_rmsd(mol, molref, coord_ref):
     coords = get_coords(mol)
     match = mol.GetSubstructMatch(molref, useChirality=False)
     nmatch = len(match)
+    match_ratio = nmatch / len(coord_ref)
     if nmatch > 0:
         rmsd = np.sqrt(((coords[match, :] - coord_ref[:nmatch])**2).mean(axis=0).sum())
-        print(mol.GetProp("_Name"), nmatch, rmsd)
+        # print(mol.GetProp("_Name"), nmatch, rmsd)
     else:
-        print(mol.GetProp("_Name"), nmatch, 9999.99)
+        rmsd = 9999.99
+        # print(mol.GetProp("_Name"), nmatch, 9999.99)
+    molname = mol.GetProp("_Name")
+    return rmsd, match_ratio, molname
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--mol2ref", help="Reference mol2 file")
+    parser.add_argument("--sdf", help="sdf file with molecules to compare with references")
+    parser.add_argument("--test", help="Test the code", action="store_true")
+    parser.add_argument("--func", help="Test only the given function(s)", nargs="+")
+    args = parser.parse_args()
+
+    molref = Chem.MolFromMol2File(args.mol2, sanitize=True)
+    suppl = Chem.SDMolSupplier(args.sdf, sanitize=True)
+
+    coord_ref = get_coords(molref)
+
+    for mol in suppl:
+        rmsd, match_ratio, molname = get_rmsd(mol, molref, coord_ref)
+        print(f"{molname=}")
+        print(f"{match_ratio=:.3g}")
+        print(f"{rmsd=:.4g}")
+        print("--")
