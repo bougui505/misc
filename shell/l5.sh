262682a12536c93a6d61e4f35a8d0a560fed8eca
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2024-12-18 13:31:31 +0100

    -n, --num, number of last files to display
    modified:   l5.sh

--- shell/l5.sh
+++ shell/l5.sh
@@ -24,17 +24,20 @@ List all the files modified the last 5 minutes (using find)
     -t, --time change the default delay time (5 minutes) to the given number of minutes
     -a, --all also display hidden files
     -d, --day, number of day to put the delay (1 day = 1440 minutes)
+    -n, --num, number of last files to display
 EOF
 }
 
 DELAY=5  # delay in minutes
 ALL=0
 DAYS=1
+NUM=0
 while [ "$#" -gt 0 ]; do
     case $1 in
         -t|--time) DELAY="$2"; shift ;;
         -d|--day) DAYS="$2"; DELAY=$(qalc -t "$DAYS * 1440") ; shift ;;
         -a|--all) ALL=1 ;;
+        -n|--num) NUM="$2"; shift ;;
         -h|--help) usage; exit 0 ;;
         --) OTHER="${@:2}";break; shift;;  # Everything after the '--' symbol
         *) usage; exit 1 ;;
@@ -42,10 +45,18 @@ while [ "$#" -gt 0 ]; do
     shift
 done
 
-FILES=$(find -L . -type f -mmin -$DELAY | sed 's,./,,') 
+if [ $NUM -gt 0 ]; then
+    FILES=$(find -L . -type f | sed 's,./,,')
+else
+    FILES=$(find -L . -type f -mmin -$DELAY | sed 's,./,,')
+fi
 if [ $ALL -eq 0 ]; then
     FILES=$(echo $FILES | tr " " "\n" | grep -v -e "^\." -e "/\." -e "__pycache__")
 fi
 if [[ ! -z $FILES ]]; then
-    echo $FILES | xargs lt
+    if [ $NUM -gt 0 ]; then
+        echo $FILES | xargs ls -rth | tail -n $NUM | xargs lt
+    else
+        echo $FILES | xargs lt
+    fi
 fi
