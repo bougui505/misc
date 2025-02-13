62a2c203d3ab931cce10c8058ed79bef1d2a80fa
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2025-02-13 11:41:34 +0100

    new file:   pigz.sh

---
+++ singularity/pigz.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2025 Institut Pasteur                                       #
+#############################################################################
+#
+# creation_date: Thu Feb 13 11:40:27 2025
+
+set -e  # exit on error
+set -o pipefail  # exit when a process in the pipe fails
+set -o noclobber  # prevent overwritting redirection
+
+# Full path to the directory of the current script
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+
+$DIRSCRIPT/bougui.sif pigz $@
