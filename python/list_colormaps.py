24ec576aaa254d91c129952c1dae1c47fc3c6703
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2025-03-07 13:17:00 +0100

    new file:   list_colormaps.py

---
+++ python/list_colormaps.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2025 Institut Pasteur                                       #
+#############################################################################
+#
+# creation_date: Fri Mar  7 13:13:59 2025
+
+import matplotlib.pyplot as plt
+
+for cmap in plt.colormaps:
+    print(cmap)
