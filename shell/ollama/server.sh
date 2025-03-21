1b9ea8a8535851a8152ce395c906dbadf95442bb
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2025-03-21 10:39:30 +0100

    modified:   server.sh

--- shell/ollama/server.sh
+++ shell/ollama/server.sh
@@ -14,7 +14,7 @@ set -o noclobber  # prevent overwritting redirection
 
 # Full path to the directory of the current script
 DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
-SIFFILE=$(realpath ollama.sif)
+SIFFILE=$(realpath $DIRSCRIPT/ollama.sif)
 echo $SIFFILE
 
 apptainer run --nv $SIFFILE serve
