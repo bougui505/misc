d6a45500d7727d444541fdd6fd4e0c21c4e72e0d
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2025-03-21 17:57:49 +0100

    Store ollama results in ~/Documents/ollama-ouputs
    modified:   ollama.sh

--- shell/ollama/ollama.sh
+++ shell/ollama/ollama.sh
@@ -12,13 +12,23 @@ set -e  # exit on error
 set -o pipefail  # exit when a process in the pipe fails
 set -o noclobber  # prevent overwritting redirection
 
+OLLAMA_OUPUT_DIR="$HOME/Documents/ollama-ouputs"
+PROMPT="$1"
+OUTFILENAME="$OLLAMA_OUPUT_DIR/$(echo $PROMPT | sed 's/ /_/g ; s/?//g').md"
+
+if [[ -f $OUTFILENAME ]]; then
+  batcat -p $OUTFILENAME
+  exit 0
+fi
+
 tsp -C
+
+mkdir -p $OLLAMA_OUPUT_DIR
 
 # Full path to the directory of the current script
 DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
-trap "tsp -k $TSPID && (cat /tmp/ollama.out | xclip) && echo 'ANSWER COPIED TO CLIPBOARD'" EXIT INT
+trap "tsp -k $TSPID && (cat $OUTFILENAME | xclip) && echo 'ANSWER COPIED TO CLIPBOARD'" EXIT INT
 
-PROMPT="$1"
 if [ ! -t 0 ]; then
   PROMPT+="\n$(</dev/stdin)"
 fi
@@ -27,5 +37,5 @@ TSPID=$(tsp $DIRSCRIPT/server.sh)
 sleep 0.5
 
 # $DIRSCRIPT/ollama run llama2 "$PROMPT"
-$DIRSCRIPT/ollama run custom "$PROMPT" --nowordwrap | tee /tmp/ollama.out
+$DIRSCRIPT/ollama run custom "$PROMPT" --nowordwrap | tee $OUTFILENAME
 # $DIRSCRIPT/ollama run codellama "$PROMPT"
