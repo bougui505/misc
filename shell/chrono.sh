c8e3cee3415212bd4aa7e4750c16d7765fc97e04
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2025-04-16 15:23:23 +0200

    modified:   chrono.sh

--- shell/chrono.sh
+++ shell/chrono.sh
@@ -30,6 +30,8 @@ Options:
     -e, --elapsed print elapsed time since the start of the script
     -m, --medium <number><ms|s|m|h> set the medium time when the time is printed in orange (ms for milliseconds, s for seconds, m for minutes, h for hours)
         default: 1m
+    -d, --duration <number><ms|s|m|h> set the estimated duration of the script in (ms for milliseconds, s for seconds, m for minutes, h for hours) to calculate estimated progress
+        default: 0s (unknown)
     -H, --high <number><ms|s|m|h> set the high time when the time is printed in red (ms for milliseconds, s for seconds, m for minutes, h for hours)
         default: 5m
 EOF
@@ -38,11 +40,13 @@ EOF
 ELAPSED=0  # Default value
 MEDIUM=1m  # medium time when the time is printed in orange (ms for milliseconds, s for seconds, m for minutes, h for hours)
 HIGH=5m  # high time when the time is printed in red (ms for milliseconds, s for seconds, m for minutes, h for hours)
+DURATION=0s  # Estimated duration of the script in seconds to calculate progess, default is 0s (unknown)
 while [ "$#" -gt 0 ]; do
     case $1 in
         -e|--elapsed) ELAPSED=1 ;;
         -m|--medium) shift; MEDIUM="$1" ;;
         -H|--high) shift; HIGH="$1" ;;
+        -d|--duration) shift; DURATION="$1" ;;
         -h|--help) usage; exit 0 ;;
         --) OTHER="${@:2}";break; shift;;  # Everything after the '--' symbol
         *) usage; exit 1 ;;
@@ -76,6 +80,19 @@ else
     echo "Error: HIGH is not in the format <number><s|m|h>" >&2
     exit 1
 fi
+# Convert DURATION to milliseconds
+if grep -q 'ms' <<< "$DURATION"; then
+    DURATION=${DURATION//ms/}
+elif [[ $DURATION =~ ^([0-9]+)([smh])$ ]]; then
+    case ${BASH_REMATCH[2]} in
+        s) DURATION=$(( ${BASH_REMATCH[1]} * 1000 )) ;;
+        m) DURATION=$(( ${BASH_REMATCH[1]} * 60000 )) ;;
+        h) DURATION=$(( ${BASH_REMATCH[1]} * 3600000 )) ;;
+    esac
+else
+    echo "Error: DURATION is not in the format <number><s|m|h>" >&2
+    exit 1
+fi
 
 function print_line () {
     echo "$deltat $line"
@@ -91,6 +108,15 @@ while sleep 0.001s; do
     t1=$(date +%s%3N)
     deltat_ms=$((t1 - t0))
     deltat=$(printf "%02d:%02d:%02d.%03d" $((deltat_ms/3600000)) $(( (deltat_ms%3600000)/60000 )) $(( (deltat_ms%60000)/1000 )) $(( deltat_ms%1000 )))
+    TOTALTIME=$(($t1 - $T0))
+    progress=""
+    if [[ DURATION -gt 0 ]]; then
+        # Calculate the progress
+        progress="$((100 * TOTALTIME / DURATION))%"
+        # format progress with a fixed width of 4 characters
+        progress=$(printf "%4s" "$progress")
+    fi
+    deltat="$deltat $progress"
     if [[ $deltat_ms -gt $MEDIUM && $deltat_ms -le $HIGH ]]; then
         # format deltat in orange
         deltat=$(echo -ne "\033[0;33m$deltat\033[0m")
@@ -104,7 +130,6 @@ while sleep 0.001s; do
     read -r -t 0.001 line
     ret=$?
     if [[ $ret -eq 1 ]]; then  # this is the end of the input
-        TOTALTIME=$(($t1 - $T0))
         # format TOTALTIME in HH:MM:SS.mmm
         TOTALTIME=$(printf "%02d:%02d:%02d.%03d" $((TOTALTIME/3600000)) $(( (TOTALTIME%3600000)/60000 )) $(( (TOTALTIME%60000)/1000 )) $(( TOTALTIME%1000 )))
         # echo the total time in cyan and underlined
