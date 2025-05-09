7506b5f5c7e8855355984fa239bae9584c89aede
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2025-05-09 11:35:27 +0200

    modified:   molprop.py

--- python/mols/molprop.py
+++ python/mols/molprop.py
@@ -7,9 +7,17 @@
 #
 # creation_date: Fri May  9 11:14:02 2025
 
+import os
+import sys
+
 import typer
 from rdkit import Chem
-from rdkit.Chem import QED
+from rdkit.Chem import QED, RDConfig
+
+# See: https://mattermodeling.stackexchange.com/a/8544
+sys.path.append(os.path.join(RDConfig.RDContribDir, 'SA_Score'))
+# now you can import sascore!
+import sascorer
 
 app = typer.Typer(
     no_args_is_help=True,
@@ -47,9 +55,32 @@ def qed():
         except:
             continue
 
+@app.command()
+def sascore():
+    """
+    Compute the SAScore (Synthetic Accessibility Score) from the
+    SMILES file given in the standard input.
+    Characterize molecule synthetic accessibility as a score between 1 (easy to make) and 10 (very difficult to make).
+    See: https://github.com/rdkit/rdkit/tree/master/Contrib/SA_Score
+    See: https://mattermodeling.stackexchange.com/a/8544
+    See: https://jcheminf.biomedcentral.com/articles/10.1186/1758-2946-1-8
+    """
+    for i, line in enumerate(sys.stdin):
+        line = line.strip()
+        smiles = line.split()[0]
+        mol = Chem.MolFromSmiles(smiles)
+        if mol is None:
+            continue
+        try:
+            Chem.SanitizeMol(mol)
+            # Compute SAScore using sascorer
+            sascore = sascorer.calculateScore(mol)
+            print(f"{line} {sascore:.3f}")
+        except:
+            continue
+
 if __name__ == "__main__":
     import doctest
-    import sys
 
     @app.command()
     def test():
