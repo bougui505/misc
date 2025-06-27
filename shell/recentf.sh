c6126b73dabd9f15b04f8641fb30f25aa14594dd
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2025-06-27 16:52:33 +0200

    new file:   recentf.sh

---
+++ shell/recentf.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2025 Institut Pasteur                                       #
+#############################################################################
+#
+# creation_date: Fri Jun 27 16:50:37 2025
+
+set -e  # exit on error
+set -o pipefail  # exit when a process in the pipe fails
+set -o noclobber  # prevent overwritting redirection
+
+# Full path to the directory of the current script
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+# MYTMP=$(mktemp -d)  # Temporary directory for the current script. Use it to put temporary files.
+# trap 'rm -rvf "$MYTMP"' EXIT INT  # Will be removed at the end of the script
+
+find . -type f -not -path '*/.*' -print0 | xargs -0 stat --format '%Y :%y %n' | sort -nr | cut -d: -f2-
