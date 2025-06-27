6018acab4d690d266ce35bfa8364a551287a8504
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2025-06-27 16:35:54 +0200

    modified:   ../sample_SMILES.py

--- python/mols/sample_SMILES.py
+++ python/mols/sample_SMILES.py
@@ -11,6 +11,8 @@
 To generate all possible SMILES strings that represent the same molecule (i.e., all valid SMILES encodings for the same molecular structure) using RDKit, you need to enumerate all possible atom orderings and generate the SMILES for each. RDKit provides a way to randomize the atom ordering and output canonical or non-canonical SMILES.
 """
 
+import sys
+
 from rdkit import Chem
 
 
@@ -23,7 +25,9 @@ def enumerate_smiles(smiles, num=1000):
     return list(smiles_set)
 
 # Example usage
-original_smiles = "NC(CCC(=O)NC(CSCC(O)CCc1ccccc1)C(=O)N=CC(=O)O)C(=O)O"
+# original_smiles = "NC(CCC(=O)NC(CSCC(O)CCc1ccccc1)C(=O)N=CC(=O)O)C(=O)O"
+# original_smiles = "OC(=O)c1ccc(cc1)c2nn(C(=O)c3c(Cl)cccc3C(F)(F)F)c4ccccc24"
+original_smiles = sys.argv[1]
 all_smiles = enumerate_smiles(original_smiles, num=2000)
 print(all_smiles)
 print(f"Generated {len(all_smiles)} unique SMILES.")
