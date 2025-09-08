dc264c1b74a7c7cabe15c23973bb128fb49a8240
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2025-09-08 09:52:56 +0200

    feat: add script to list available GPUs

---
+++ shell/list_gpu.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2025 Institut Pasteur                                       #
+#############################################################################
+#
+# creation_date: Mon Sep  8 09:50:22 2025
+
+set -e  # exit on error
+set -o pipefail  # exit when a process in the pipe fails
+set -o noclobber  # prevent overwritting redirection
+
+# Full path to the directory of the current script
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+# MYTMP=$(mktemp -d)  # Temporary directory for the current script. Use it to put temporary files.
+# trap 'rm -rvf "$MYTMP"' EXIT INT  # Will be removed at the end of the script
+
+function usage () {
+    cat << EOF
+Help message
+    -h, --help print this help message and exit
+EOF
+}
+
+HOST="maestro"  # Default value
+while [ "$#" -gt 0 ]; do
+    case $1 in
+        --host) HOST="$2"; shift ;;
+        -h|--help) usage; exit 0 ;;
+        --) OTHER="${@:2}";break; shift;;  # Everything after the '--' symbol
+        *) usage; exit 1 ;;
+    esac
+    shift
+done
+
+ssh $HOST 'module load slurm;sinfo --format="%30N|%.22f|%.20G|%.10A|%.15E"'
