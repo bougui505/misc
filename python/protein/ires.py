3d54cd55a22d1267a04dea31add53cd316691ea1
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2025-09-11 10:02:09 +0200

    feat: add protein ires module

---
+++ python/protein/ires.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2025 Institut Pasteur                                       #
+#############################################################################
+#
+# creation_date: Thu Sep 11 09:47:28 2025
+
+import typer
+import os
+from pymol2 import PyMOL
+
+# import IPython  # you can use IPython.embed() to explore variables and explore where it's called
+
+# app = typer.Typer(
+#     no_args_is_help=True,
+#     pretty_exceptions_show_locals=False,  # do not show local variable
+#     add_completion=False,
+# )
+
+def get_interface(
+    pdb:str
+    ):
+    with PyMOL() as pml:
+        if os.path.isfile(pdb):
+            pml.cmd.load(pdb)
+        else:
+            pml.cmd.fetch(pdb)
+
+if __name__ == "__main__":
+    typer.run(get_interface)
+
