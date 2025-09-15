e7e6835d204a1046ad8223ee8713e084556b3740
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2025-09-15 10:00:19 +0200

    feat: Add PyMOL caption module

---
+++ python/pymol/caption.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2025 Institut Pasteur                                       #
+#############################################################################
+#
+# creation_date: Mon Sep 15 09:58:44 2025
+
+from pymol import cmd
+import os
+
+# AI! define the pdb download path to ~/pdb
+
+def loader(pdb):
+    pass
