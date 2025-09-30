d1c4c5774fc0b553c7df479bf1189b257202be3c
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2025-09-30 10:32:13 +0200

    feat: Add script to generate and view notes as HTML

---
+++ notes.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2025 Institut Pasteur                                       #
+#############################################################################
+#
+# creation_date: Tue Sep 30 10:23:09 2025
+
+set -e  # exit on error
+set -o pipefail  # exit when a process in the pipe fails
+# set -o noclobber  # prevent overwriting redirection
+
+# Full path to the directory of the current script
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+# MYTMP=$(mktemp -d)  # Temporary directory for the current script. Use it to put temporary files.
+# trap 'rm -rvf "$MYTMP"' EXIT INT  # Will be removed at the end of the script
+
+function usage () {
+    cat << EOF
+Help message
+    -h, --help print this help message and exit
+EOF
+}
+
+# N=1  # Default value
+while [ "$#" -gt 0 ]; do
+    case $1 in
+        # -n|--number) N="$2"; shift ;;
+        -h|--help) usage; exit 0 ;;
+        --) OTHER="${@:2}";break; shift;;  # Everything after the '--' symbol
+        *) usage; exit 1 ;;
+    esac
+    shift
+done
+
+pandoc -f markdown -t html $DIRSCRIPT/NOTES.md > $DIRSCRIPT/notes.html
+google-chrome --new-window $DIRSCRIPT/notes.html
