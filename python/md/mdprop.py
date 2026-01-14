f12923b9fd3536dc6854cd431b3f597407738730
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2026-01-14 09:40:14 +0100

    feat: add mdprop.py module for molecular dynamics properties

---
+++ python/md/mdprop.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2025 Institut Pasteur                                       #
+#############################################################################
+#
+# creation_date: Wed Jan 14 09:39:24 2026
+
+import typer
+
+# import IPython  # you can use IPython.embed() to explore variables and explore where it's called
+
+app = typer.Typer(
+    no_args_is_help=True,
+    pretty_exceptions_show_locals=False,  # do not show local variable
+    add_completion=False,
+)
+
+@app.callback()
+def callback(debug:bool=False):
+    """
+    This is a template file for a Python script using Typer.
+    It contains a main function and a test function.
+    """
+    global DEBUG
+    DEBUG = debug
+    app.pretty_exceptions_show_locals = debug
+
+if __name__ == "__main__":
+    import doctest
+    import sys
+
+    @app.command()
+    def test():
+        """
+        Test the code
+        """
+        doctest.testmod(
+            optionflags=doctest.ELLIPSIS \
+                        | doctest.REPORT_ONLY_FIRST_FAILURE \
+                        | doctest.REPORT_NDIFF
+        )
+
+    @app.command()
+    def test_func(func:str):
+        """
+        Test the given function
+        """
+        print(f"Testing {func}")
+        f = getattr(sys.modules[__name__], func)
+        doctest.run_docstring_examples(
+            f,
+            globals(),
+            optionflags=doctest.ELLIPSIS \
+                        | doctest.REPORT_ONLY_FIRST_FAILURE \
+                        | doctest.REPORT_NDIFF,
+        )
+
+    app()
