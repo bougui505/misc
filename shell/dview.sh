5c47594cb1f0428c0f6f3743c02c74ccb516e170
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2026-05-20 10:30:19 +0200

    Open DataWarrior with a simple table view by default using .dwar format

--- shell/dview.sh
+++ shell/dview.sh
@@ -6,18 +6,31 @@ MYTMP=$(mktemp -d)
 
 trap 'rm -rvf "$MYTMP"' EXIT INT  # Will be removed at the end of the script
 
-tmpfile=$MYTMP/smi.csv
-# Read stdin and dump it into the temp file
-cat | tr " " "," \
-  | awkcsv '
-    {
-      if (NR==1){
-        for (i=1;i<=NF;i++){
-          printf "c%d%s", i, (i<NF ? "," : "\n")
+tmpfile=$MYTMP/smi.dwar
+# Read stdin, convert spaces to tabs, write the header/data, and append properties
+(
+  cat | tr " " "\t" \
+    | awk '
+      BEGIN { FS="\t"; OFS="\t" }
+      {
+        if (NR==1){
+          for (i=1;i<=NF;i++){
+            printf "c%d%s", i, (i<NF ? "\t" : "\n")
+          }
         }
-      }
-      print $0
-    }' > $tmpfile
-echo $tmpfile
+        print $0
+      }'
+  cat <<'EOF'
+<datawarrior properties>
+<mainView="Table">
+<mainViewCount="1">
+<mainViewDockInfo0="root">
+<mainViewName0="Table">
+<mainViewType0="tableView">
+</datawarrior properties>
+EOF
+) > "$tmpfile"
+
+echo "$tmpfile"
 # Open DataWarrior with the file in the background, then clean up the file
 datawarrior "$tmpfile"
